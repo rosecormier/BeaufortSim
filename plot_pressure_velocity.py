d10f8f2765e0600230830b7ea64121afd783e108
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-05-15 11:01:45 -0400

    Set global variables and create directories

--- plot_pressure_velocity.py
+++ plot_pressure_velocity.py
@@ -1,12 +1,21 @@
+"""
+Rosalie Cormier, 2023
+"""
+
+#IMPORTS
+
 import sys
+import os
 import argparse
 import matplotlib.pyplot as plt
+
+from os.path import expanduser, join
 
 from ecco_visualization import *
 
 ###############
 
-#Parse command-line input
+#PARSE COMMAND-LINE INPUT AND SET GLOBAL VARIABLES
 
 parser = argparse.ArgumentParser(description="Plot pressure and velocity fields in Beaufort Gyre",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -16,12 +25,29 @@ parser.add_argument("--lons", type=float, help="Bounding longitudes", nargs=2, d
 parser.add_argument("--month", type=str, help="Start month", default="01")
 parser.add_argument("--months", type=int, help="Total number of months", default=12)
 parser.add_argument("--kvals", type=int, help="Bounding k-values", nargs=2, default=[0, 4])
-parser.add_argument("--datdir", type=str, help="Directory (relative to home) to store ECCO data", default="/Downloads/")
-parser.add_argument("--outdir", type=str, help="Output directory (relative to here)", default="/visualization/")
+parser.add_argument("--datdir", type=str, help="Directory (relative to home) to store ECCO data", default="Downloads")
+parser.add_argument("--outdir", type=str, help="Output directory (relative to here)", default="visualization")
 
 parser.add_argument("start", type=int, help="Start year")
 
 args = parser.parse_args()
 
 config = vars(args)
-print(config)
+
+latmin, latmax = config['lats'][0], config['lats'][1]
+lonmin, lonmax = config['lons'][0], config['lons'][1]
+startmo, startyr, mos = config['month'], config['start'], config['months']
+kmin, kmax = config['kvals'][0], config['kvals'][1]
+
+user_home_dir = expanduser('~')
+sys.path.append(join(user_home_dir, 'ECCOv4-py'))
+
+datdir = join(user_home_dir, config['datdir'], 'ECCO_V4r4_PODAAC')
+
+if not os.path.exists(datdir):
+    os.makedirs(datdir)
+    
+outdir = join(".", config['outdir'])
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
