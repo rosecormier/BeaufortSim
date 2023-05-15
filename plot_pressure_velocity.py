ba2d281d2c105b501917ed97a3831af534d4d245
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-05-15 11:35:02 -0400

    Add ecco_download file and download functionality

--- plot_pressure_velocity.py
+++ plot_pressure_velocity.py
@@ -2,14 +2,18 @@
 Rosalie Cormier, 2023
 """
 
+##############################
+
 #IMPORTS AND SETTINGS
 
 import sys
 import os
 import argparse
+import ecco_v4_py as ecco
 import matplotlib.pyplot as plt
 
 from os.path import expanduser, join
+from ecco_download import ecco_podaac_download
 
 from ecco_visualization import *
 
@@ -53,3 +57,43 @@ outdir = join(".", config['outdir'])
 
 if not os.path.exists(outdir):
     os.makedirs(outdir)
+    
+month_dict = {0: "01", 1: "02", 2: "03", 3: "04", 4: "05", 5: "06",
+              6: "07", 7: "08", 8: "09", 9: "10", 10: "11", 11: "12"}
+month_key_list = list(month_dict.keys())
+month_val_list = list(month_dict.values())
+month_end_dict = {"01": "31", "02": "28", "03": "31", "04": "30",
+                 "05": "31", "06": "30", "07": "31", "08": "31",
+                 "09": "30", "10": "31", "11": "30", "12": "31"} #Ideally fix Feb. at some point
+    
+##############################
+
+#DOWNLOAD ECCO FILES FOR SPECIFIED MONTHS
+
+vel_monthly_shortname = "ECCO_L4_OCEAN_VEL_LLC0090GRID_MONTHLY_V4R4"
+denspress_monthly_shortname = "ECCO_L4_DENS_STRAT_PRESS_LLC0090GRID_MONTHLY_V4R4"
+grid_params_shortname = "ECCO_L4_GEOMETRY_LLC0090GRID_V4R4"
+
+month_index = month_val_list.index(startmo)
+i = month_key_list[month_index]
+
+while i < mos:
+    
+    month = month_dict[i % 12]
+    endmonth = month_end_dict[month]
+    
+    #Download the monthly-averaged velocity file
+    ecco_podaac_download(ShortName=vel_monthly_shortname, StartDate="2000-"+month+"-02", 
+                         EndDate="2000-"+month+"-"+endmonth, download_root_dir=datdir, n_workers=6, 
+                         force_redownload=False)
+    
+    #Download the monthly-averaged density-/pressure-anomaly file
+    ecco_podaac_download(ShortName=denspress_monthly_shortname, StartDate="2000-"+month+"-02", 
+                         EndDate="2000-"+month+"-"+endmonth, download_root_dir=datdir, n_workers=6, 
+                         force_redownload=False)
+    
+    i += 1 #Next month
+    
+#Download ECCO grid parameters
+ecco_podaac_download(ShortName=grid_params_shortname, StartDate="2000-01-01", EndDate="2000-01-01", 
+                     download_root_dir=datdir, n_workers=6, force_redownload=False)
