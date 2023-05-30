c9aa0bcff524bc622cfb9c069f3884c46f698c86
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-05-30 15:51:14 -0400

    Write function to compute Okubo-Weiss parameter

--- vorticity_functions.py
+++ vorticity_functions.py
@@ -5,14 +5,9 @@ Vorticity-related functions
 """
 
 import numpy as np
-#import xarray
 import dask.array as daskarray
 from matplotlib import pyplot as plt
-#%matplotlib inline
-#from matplotlib.colors import SymLogNorm
-#from xmitgcm import open_mdsdataset
 import xgcm
-#from mpl_toolkits.basemap import Basemap, cm, shiftgrid
 import xarray as xr
 
 def comp_vorticity(grid_llc, mean_u, mean_v, dxC, dyC, cell_area, k_val):
@@ -21,3 +16,17 @@ def comp_vorticity(grid_llc, mean_u, mean_v, dxC, dyC, cell_area, k_val):
     zeta = zeta.isel(k=k_val).squeeze()
     
     return zeta
+
+def comp_OkuboWeiss(grid_llc, mean_u, mean_v, dxC, dyC, cell_area, k_val):
+    
+    omega = comp_vorticity(grid_llc, mean_u, mean_v, dxC, dyC, cell_area, k_val)
+    
+    normal_strain = (grid_llc.diff(mean_u * dyC, 'X') - grid_llc.diff(mean_v * dxC, 'Y')) / cell_area
+    normal_strain = normal_strain.isel(k=k_val).squeeze()
+    
+    shear_strain = (grid_llc.diff(mean_v * dyC, 'X') + grid_llc.diff(mean_u * dxC, 'Y')) / cell_area
+    shear_strain = shear_strain.isel(k=k_val).squeeze()
+    
+    W = normal_strain**2 + shear_strain**2 - omega**2
+    
+    return W
