8597fe60cbc713515015e55f25d336c7f9a6ed0a
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-06-08 10:09:56 -0400

    Rename variables in vorticity_functions

--- vorticity_functions.py
+++ vorticity_functions.py
@@ -12,14 +12,14 @@ import ecco_v4_py as ecco
 
 from ecco_general import ecco_resample
 
-def comp_vorticity(grid_llc, mean_u, mean_v, dx, dy, cell_area):
-    return (-grid_llc.diff(mean_u * dx, 'Y') + grid_llc.diff(mean_v * dy, 'X')).squeeze() / cell_area
+def comp_vorticity(grid_llc, u_mean, v_mean, dx, dy, cell_area):
+    return (-grid_llc.diff(u_mean * dx, 'Y') + grid_llc.diff(v_mean * dy, 'X')).squeeze() / cell_area
 
-def comp_normal_strain(grid_llc, mean_u, mean_v, dxG, dyG, cell_area):
-    return (grid_llc.diff(mean_u * dyG, 'X') - grid_llc.diff(mean_v * dxG, 'Y')) / cell_area
+def comp_normal_strain(grid_llc, u_mean, v_mean, dx, dy, cell_area):
+    return (grid_llc.diff(u_mean * dy, 'X') - grid_llc.diff(v_mean * dx, 'Y')) / cell_area
 
-def comp_shear_strain(grid_llc, mean_u, mean_v, dxC, dyC, cell_area):
-    return (grid_llc.diff(mean_v * dyC, 'X') + grid_llc.diff(mean_u * dxC, 'Y')) / cell_area
+def comp_shear_strain(grid_llc, u_mean, v_mean, dx, dy, cell_area):
+    return (grid_llc.diff(v_mean * dy, 'X') + grid_llc.diff(u_mean * dx, 'Y')) / cell_area
 
 def comp_total_strain(ds_grid, normal_strain, shear_strain, latmin, latmax, lonmin, lonmax, resolution):
 
@@ -29,5 +29,4 @@ def comp_total_strain(ds_grid, normal_strain, shear_strain, latmin, latmax, lonm
     return normal_strain_field + shear_strain_field
 
 def comp_OkuboWeiss(omega, normal_strain, shear_strain):
-
     return normal_strain**2 + shear_strain**2 - omega**2
