c30935ce3179089ee2198bfea181bfa00f7e4e39
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-06-13 16:49:19 -0400

    Set scalar and vector attributes to be input parameters

--- plot_scalar_and_vector.py
+++ plot_scalar_and_vector.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from os.path import expanduser, join
 
-from ecco_general import load_grid, get_monthstr, load_dataset, ds_to_field, comp_residuals, get_starting_i, rotate_vector
+from ecco_general import load_grid, get_vector_partner, get_monthstr, load_dataset, ds_to_field, comp_residuals, get_starting_i, rotate_vector
 from ecco_visualization import cbar_label, contourf_quiver_title, ArcCir_contourf_quiver, ArcCir_contourf_quiver_grid
 from ecco_field_variables import get_scalar_field_vars, get_vector_field_vars
 
@@ -38,6 +38,8 @@ parser.add_argument("--lons", type=float, help="Bounding longitudes", nargs=2, \
 parser.add_argument("--month", type=str, help="Start month", default="01")
 parser.add_argument("--months", type=int, help="Total number of months", default=12)
 parser.add_argument("--kvals", type=int, help="Bounding k-values", nargs=2, default=[0, 1])
+parser.add_argument("--scalar", type=str, help="Name of scalar attribute", default="PHIHYDcR")
+parser.add_argument("--xvec", type=str, help="Name of vector attribute (x-comp)", default="UVEL")
 parser.add_argument("--vminmax", type=float, help="Minimum/maximum scalar values", nargs=2, \
                     default=[-1, 1])
 parser.add_argument("--res", type=float, help="Lat/lon resolution in degrees", nargs=1, \
@@ -48,6 +50,7 @@ parser.add_argument("--outdir", type=str, help="Output directory (rel. to here)"
                     default="visualization")
 parser.add_argument("--seasonal", type=bool, help="Whether to take seasonal averages rather than continuous averages", \
                     default=False)
+parser.add_argument("--season", type=str, help="Months marking start and end of 'season'", default=["01", "01"])
 
 parser.add_argument("start", type=int, help="Start year")
 
@@ -56,10 +59,20 @@ config = vars(args)
 
 latmin, latmax = config['lats'][0], config['lats'][1]
 lonmin, lonmax = config['lons'][0], config['lons'][1]
-startmo, startyr, mos = config['month'], config['start'], config['months']
-kmin, kmax = config['kvals'][0], config['kvals'][1]
-scalar_bounds = config['vminmax']
 resolution = config['res']
+
+kmin, kmax = config['kvals'][0], config['kvals'][1]
+
+scalar_attr = config['scalar']
+xvec_attr = config['xvec']
+yvec_attr = get_vector_partner(xvec_attr)
+scalar_bounds = config['vminmax']
+
+startmo, startyr, mos = config['month'], config['start'], config['months']
+seasonal = config['seasonal']
+
+if seasonal:
+    season_start, season_end = config['season'][0], config['season'][1]
 
 user_home_dir = expanduser('~')
 sys.path.append(join(user_home_dir, 'ECCOv4-py'))
@@ -69,11 +82,6 @@ outdir = join(".", config['outdir'])
 
 if not os.path.exists(outdir):
     os.makedirs(outdir)
-
-#Set parameters and get associated variables
-
-scalar_attr = 'PHIHYDcR'
-xvec_attr, yvec_attr = 'UVEL', 'VVEL'
 
 vector_monthly_shortname, vector_monthly_nc_str, vector_variable = \
     get_vector_field_vars(xvec_attr, yvec_attr)
