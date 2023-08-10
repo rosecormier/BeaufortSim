3715b018f23989595a5c7c68e51efc533c527c34
francispoulin <fpoulin@uwaterloo.a>
2023-08-10 15:37:06 -0400

    create class in read_input.py

--- read_input.py
+++ read_input.py
@@ -1,6 +1,14 @@
 f = open('input.txt', 'r')
 
-import sys
+import numpy as np
+import os, sys
+
+class Parameters():
+
+    initial_date = None
+    final_date   = None
+
+param_data = Parameters
 
 for line in f:
 
@@ -19,10 +27,34 @@ for line in f:
 
         try:
             val = float(line[var_len+1:len_len-1].strip())
+            val = np.array(np.matrix(val)).ravel()
         except:
             val = line[var_len+1:line_len].strip()
 
-        print(var, val)
-        #setattr(param_data, var, val)
-        sys.exit()
+
+        setattr(param_data, var, val)
+
 f.close
+
+print(param_data.initial_date)
+print(param_data.final_date)
+
+print(param_data.time_ave_type)
+print(param_data.monthly_range)
+
+print(param_data.lat_range)
+print(param_data.lon_range)
+print(param_data.depth_range)
+print(param_data.lat_res)
+print(param_data.lon_res)
+print(param_data.interp_type)
+
+print(param_data.plot_plane_type)
+print(param_data.k_plot)
+print(param_data.lat_plot)
+print(param_data.lon_plot)
+
+print(param_data.plot_fields)
+print(param_data.data_folder_primary)
+print(param_data.data_folder_secondary)
+print(param_data.visualization_folder)
