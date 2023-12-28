c05d9cc20dfbb5d15813e222b13d9de22f21a0c2
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-12-28 17:41:44 -0500

    Update file description

--- download_data.py
+++ download_data.py
@@ -1,5 +1,5 @@
 """
-For a specified ECCO field and date range, iterates over time and:
+For a specified ECCO field (scalar or vector) and date range, iterates over time and:
     -Checks whether ECCO-field data is already downloaded; and
         -If not, downloads the data.
 
