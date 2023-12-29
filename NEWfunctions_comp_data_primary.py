c2b6a87d53ec66528e413398681578d6f73db35c
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-12-28 22:29:05 -0500

    Update file description

--- NEWfunctions_comp_data_primary.py
+++ NEWfunctions_comp_data_primary.py
@@ -1,1 +1,16 @@
-test
+"""
+Contains functions that directly produce computed data.
+
+To be added
+    -Geostrophic velocity
+    -Vorticity
+    -Normal strain
+    -Shear strain
+    -Horizontal vel div
+    -Ocean surface stresses
+    -Ekman velocity
+
+Only does monthly averaging at the moment; will update.
+
+Rosalie Cormier, 2023
+"""
