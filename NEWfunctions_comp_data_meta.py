b07771cf72df237258488cfb086a1326cf3a779a
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-12-28 22:22:18 -0500

    Fix data computation process for monthly-averaged data

--- NEWfunctions_comp_data_meta.py
+++ NEWfunctions_comp_data_meta.py
@@ -15,23 +15,56 @@ from functions_field_variables import get_field_variable, get_monthly_shortname,
 
 ##############################
 
-def create_comp_data_file(field_name, date_string, datdir_secondary, time_ave_type='monthly'):
+def create_comp_data_file(field_name, initial_month, initial_year, final_month, final_year, datdir_secondary, time_ave_type='monthly'):
     
     """
     Checks that a computed file exists, and creates it if it doesn't.
     Note - only does monthly avgs at the moment - will fix in future (i.e., will make time_ave_type variable)
     """
     
+    month, year = int(initial_month), int(initial_year)
+    
     if time_ave_type == 'monthly':
-        date_string = yearstr + '-' + monthstr
+    
         field_shortname = get_monthly_shortname(get_field_variable(field_name))
         field_nc_string = get_monthly_nc_string(get_field_variable(field_name))
 
-    filename = field_nc_string + date_string + '.nc'
-    path_to_file = os.path.join(datdir_secondary, field_shortname, filename)
-    
-    #if not os.path.exists(path_to_file): #Execute only if the file doesn't already exist
-        #create the file - to be added
+        while year < int(final_year):
+            
+            yearstr = str(year)
+            
+            while month <= 12:
+                
+                monthstr = get_monthstr(month-1) #The indexing is weird for months
+                date_string = yearstr + '-' + monthstr
+
+                filename = field_nc_string + date_string + '.nc'
+                path_to_file = os.path.join(datdir_secondary, field_shortname, filename)
+
+                #if not os.path.exists(path_to_file): #Execute only if the file doesn't already exist
+                    #create the file - to be added
+                
+                month += 1
+                
+            year += 1
+            month = 1
+        
+        if year == int(final_year):
+
+            while month <= int(final_month):
+
+                monthstr = get_monthstr(month-1) #The indexing is weird for months
+                date_string = yearstr + '-' + monthstr
+
+                filename = field_nc_string + date_string + '.nc'
+                path_to_file = os.path.join(datdir_secondary, field_shortname, filename)
+
+                #if not os.path.exists(path_to_file): #Execute only if the file doesn't already exist
+                    #create the file - to be added
+                
+                month += 1
+                
+    print("Done computing secondary data.")
         
 ##############################
 
