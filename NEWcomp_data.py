0be8ca65b13674b6e8811579126e6303606860e5
R. Cormier <rosalie.cormier@uwaterloo.ca>
2023-12-29 16:58:13 -0500

    Create function to compute Ekman velocity field

--- NEWcomp_data.py
+++ NEWcomp_data.py
@@ -1,13 +1,14 @@
 """
 Contains functions that directly produce computed data.
     -Geostrophic velocity
+    -Ekman velocity
 
 To be added
     -Vorticity
     -Normal strain
     -Shear strain
     -Horizontal vel div
-    -Ekman velocity/div
+    -Ekman velocity div
     
 The main function picks out the appropriate function to compute the requested field.
 
@@ -41,10 +42,10 @@ def comp_geostrophic_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_seco
     
     if time_ave_type == 'monthly':
         
-        date_string = yearstr + '-' + monthstr
-        
-        #Look for the density/pressure file in primary directory and download if it doesnt exist
+        #Look for the density/pressure file in primary directory and download if it doesn't exist
         download_data.main(field_name='density', initial_month=monthstr, initial_year=yearstr, final_month=monthstr, final_year=yearstr, time_ave_type=time_ave_type, datdir_primary=datdir_primary)
+        
+        date_string = yearstr + '-' + monthstr
         
         denspress_shortname, denspress_nc_string = get_monthly_shortname(get_field_variable('density')), get_monthly_nc_string(get_field_variable('density'))
         denspress_file = join(datdir_primary, denspress_shortname, denspress_nc_string+date_string+"_ECCO_V4r4_native_llc0090.nc")
@@ -68,8 +69,48 @@ def comp_geostrophic_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_seco
 
 ############################## 
 
+#EKMAN VELOCITY
 
+def comp_Ek_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_secondary, rho_ref, time_ave_type='monthly'):
+    
+    """
+    Computes and saves Ekman velocity components (u_Ek, v_Ek) to DataSet in NetCDF format.
+    """
 
+    if time_ave_type == 'monthly':
+        
+        #Look for the density/pressure file in primary directory and download if it doesn't exist
+        download_data.main(field_name='density', initial_month=monthstr, initial_year=yearstr, final_month=monthstr, final_year=yearstr, time_ave_type=time_ave_type, datdir_primary=datdir_primary)
+        
+        date_string = yearstr + '-' + monthstr
+        
+        denspress_shortname, denspress_nc_string = get_monthly_shortname(get_field_variable('density')), get_monthly_nc_string(get_field_variable('density'))
+        denspress_file = join(datdir_primary, denspress_shortname, denspress_nc_string+date_string+"_ECCO_V4r4_native_llc0090.nc")
+        ds_denspress = load_dataset(denspress_file) #Load the density-/pressure-anomaly DataSet into workspace
+    
+        #Call this function to extract the density and pressure from the DataSet
+        density, pressure = get_density_and_pressure(ds_denspress, rho_ref)
+        
+        #Look for the surface wind-on-ocean-stress file in primary directory and download if it doesn't exist
+        download_data.main(field_name='wind_stress', initial_month=monthstr, initial_year=yearstr, final_month=monthstr, final_year=yearstr, time_ave_type=time_ave_type, datdir_primary=datdir_primary)
+        
+        stress_shortname, stress_nc_string = get_monthly_shortname(get_field_variable('wind_stress')), get_monthly_nc_string(get_field_variable('wind_stress'))
+        stress_file = join(datdir_primary, stress_shortname, stress_nc_string+date_string+"_ECCO_V4r4_native_llc0090.nc")
+        ds_stress = load_dataset(stress_file) #Load the stress DataSet into workspace
+    
+        #Compute Ekman velocity components
+        u_Ek, v_Ek = comp_Ekman_vel(ds_grid, ds_denspress, ds_stress, nu_E, rho_ref)
+
+        #Save the data
+        
+        u_Ek.name = 'UEk'
+        v_Ek.name = 'VEk'
+        vel_Ek_ds = xr.merge([u_Ek, v_Ek])
+        vel_Ek_shortname, vel_Ek_nc_string = get_monthly_shortname(get_field_variable('Ek_vel')), get_monthly_nc_string(get_field_variable('Ek_vel'))
+        if not os.path.exists(join(datdir_secondary, vel_Ek_shortname)):
+            os.makedirs(join(datdir_secondary, vel_Ek_shortname))
+        vel_Ek_ds.to_netcdf(path=join(datdir_secondary, vel_Ek_shortname, vel_Ek_nc_string+date_string+".nc"), engine="scipy")
+            
 ##############################
 
 #each field gets a separate function
@@ -83,8 +124,7 @@ def comp_geostrophic_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_seco
 
 #vorticity
 
-#Ekman velocity, and divergence
-    #First load ocean surface stresses (primary data)
+#Ekman velocity divergence
     
 ##############################
 
@@ -108,6 +148,8 @@ def main(**kwargs):
 
     if field_name == 'geostrophic_vel':
         comp_geostrophic_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_secondary, rho_ref, time_ave_type='monthly')
+    elif field_name == 'Ek_vel':
+        comp_Ek_vel(ds_grid, monthstr, yearstr, datdir_primary, datdir_secondary, rho_ref, time_ave_type='monthly')
 
 ##############################
 
