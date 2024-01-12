b4b53220e795b02c4021e94996488cd2ea9980fc
R. Cormier <rosalie.cormier@uwaterloo.ca>
2024-01-11 20:58:58 -0500

    Fix indentation and track date strings during seasonal averaging

--- download_data.py
+++ download_data.py
@@ -57,117 +57,126 @@ def main(**kwargs):
         
         month, year = int(initial_month), int(initial_year)
     
-        if time_ave_type == "monthly":
+    if time_ave_type == "monthly":
         
-            field_shortname = get_monthly_shortname(get_field_variable(field_name))
-            field_nc_string = get_monthly_nc_string(get_field_variable(field_name))
+        field_shortname = get_monthly_shortname(get_field_variable(field_name))
+        field_nc_string = get_monthly_nc_string(get_field_variable(field_name))
     
-            while year < int(final_year):
+        while year < int(final_year):
+            yearstr = str(year)
+            while month <= 12:
+                monthstr = get_monthstr(month)
+                endmonth = get_month_end(monthstr, yearstr)
+                StartDate, EndDate = yearstr+"-"+monthstr+"-02", yearstr+"-"+monthstr+"-"+endmonth
+                ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
+                month += 1
+            year += 1
+            month = 1
+        
+        if year == int(final_year):
+            while month <= int(final_month):
+                monthstr = get_monthstr(month)
+                endmonth = get_month_end(monthstr, final_year)
+                StartDate, EndDate = final_year+"-"+monthstr+"-02", final_year+"-"+monthstr+"-"+endmonth
+                ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
+                month += 1
+                    
+        print("Done downloading ECCO data.")
+        return None
+        
+    elif time_ave_type == "seasonal":
+        
+        field_shortname = get_monthly_shortname(get_field_variable(field_name))
+        field_nc_string = get_monthly_nc_string(get_field_variable(field_name))
+            
+        season_start_string, season_end_string = time_kwargs[0], time_kwargs[1]
+        season_start, season_end = int(season_start_string), int(season_end_string)
+        
+        #This list will be used to track the dates corresponding to the files we download
+        all_seasons_all_date_strings = []
+            
+        if season_start < season_end:
+                
+            while year <= int(final_year):
+                    
                 yearstr = str(year)
-                while month <= 12:
+                monthly_fields_0, monthly_fields_1 = None, None
+                
+                while month <= season_end:
                     monthstr = get_monthstr(month)
                     endmonth = get_month_end(monthstr, yearstr)
-                    StartDate, EndDate = yearstr+"-"+monthstr+"-02", yearstr+"-"+monthstr+"-"+endmonth
+                    date_string = yearstr + "-" + monthstr
+                    StartDate, EndDate = date_string + "-02", date_string + "-" + endmonth
                     ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
+                    ds_month = load_data_files.main(field_name=field_name, date_string=date_string, datdir_primary=datdir_primary, time_ave_type='monthly') 
+                        
+                    try:
+                        attributes = get_vector_comps(field_name) #For vector fields
+                        if monthly_fields_0 is None:
+                            monthly_fields_0, monthly_fields_1 = ds_month[attributes[0]], ds_month[attributes[1]]
+                        elif monthly_fields_0 is not None:
+                            monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[attributes[0]]], 'time')
+                            monthly_fields_1 = xr.concat([monthly_fields_1, ds_month[attributes[1]]], 'time')
+                    except:
+                        if monthly_fields_0 is None:
+                            monthly_fields_0 = ds_month[get_field_variable(field_name)]
+                        elif monthly_fields_0 is not None:
+                            monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[get_field_variable(field_name)]], 'time')
+                    
+                    all_seasons_all_date_strings.append(date_string)
+                    
                     month += 1
+                        
+                #After loading data for all months in the season, compute seasonal average
+                compute_seasonal_average([monthly_fields_0, monthly_fields_1], datdir_primary, field_name, season_start_string, season_end_string, yearstr)
+                        
                 year += 1
-                month = 1
-        
-            if year == int(final_year):
-                while month <= int(final_month):
-                    monthstr = get_monthstr(month)
-                    endmonth = get_month_end(monthstr, final_year)
-                    StartDate, EndDate = final_year+"-"+monthstr+"-02", final_year+"-"+monthstr+"-"+endmonth
-                    ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
-                    month += 1
-                    
-            print("Done downloading ECCO data.")
-        
-        elif time_ave_type == "seasonal":
-        
-            field_shortname = get_monthly_shortname(get_field_variable(field_name))
-            field_nc_string = get_monthly_nc_string(get_field_variable(field_name))
-            
-            season_start_string, season_end_string = time_kwargs[0], time_kwargs[1]
-            season_start, season_end = int(season_start_string), int(season_end_string)
-            
-            if season_start < season_end:
+                month = season_start
+
+        elif season_start > season_end:
                 
-                while year <= int(final_year):
+            while year <= int(final_year):
                     
+                monthly_fields_0, monthly_fields_1 = None, None
+                    
+                while (season_start <= month) or (month <= season_end):
+                        
                     yearstr = str(year)
-                    monthly_fields_0, monthly_fields_1 = None, None
-                    
-                    while month <= season_end:
-                        monthstr = get_monthstr(month)
-                        endmonth = get_month_end(monthstr, yearstr)
-                        date_string = yearstr + "-" + monthstr
-                        StartDate, EndDate = date_string + "-02", date_string + "-" + endmonth
-                        ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
-                        ds_month = load_data_files.main(field_name=field_name, date_string=date_string, datdir_primary=datdir_primary, time_ave_type='monthly') 
+                    monthstr = get_monthstr(month)
+                    endmonth = get_month_end(monthstr, yearstr)
+                    date_string = yearstr + "-" + monthstr
+                    StartDate, EndDate = date_string + "-02", date_string + "-" + endmonth
+                    ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
+                    ds_month = load_data_files.main(field_name=field_name, date_string=date_string, datdir_primary=datdir_primary, time_ave_type='monthly') 
+
+                    try:
+                        attributes = get_vector_comps(field_name) #For vector fields
+                        if monthly_fields_0 is None:
+                            monthly_fields_0, monthly_fields_1 = ds_month[attributes[0]], ds_month[attributes[1]]
+                        elif monthly_fields_0 is not None:
+                            monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[attributes[0]]], 'time')
+                            monthly_fields_1 = xr.concat([monthly_fields_1, ds_month[attributes[1]]], 'time')
+                    except:
+                        if monthly_fields_0 is None:
+                            monthly_fields_0 = ds_month[get_field_variable(field_name)]
+                        elif monthly_fields_0 is not None:
+                            monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[get_field_variable(field_name)]], 'time')
                         
-                        try:
-                            attributes = get_vector_comps(field_name) #For vector fields
-                            if monthly_fields_0 is None:
-                                monthly_fields_0, monthly_fields_1 = ds_month[attributes[0]], ds_month[attributes[1]]
-                            elif monthly_fields_0 is not None:
-                                monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[attributes[0]]], 'time')
-                                monthly_fields_1 = xr.concat([monthly_fields_1, ds_month[attributes[1]]], 'time')
-                        except:
-                            if monthly_fields_0 is None:
-                                monthly_fields_0 = ds_month[get_field_variable(field_name)]
-                            elif monthly_fields_0 is not None:
-                                monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[get_field_variable(field_name)]], 'time')
+                    all_seasons_all_date_strings.append(date_string)
                         
+                    if month == 12:
+                        year += 1
+                        month = 1
+                    else:
                         month += 1
-                        
-                    #After loading data for all months in the season, compute seasonal average
-                    compute_seasonal_average([monthly_fields_0, monthly_fields_1], datdir_primary, field_name, season_start_string, season_end_string, yearstr)
-                        
-                    year += 1
-                    month = season_start
-
-            elif season_start > season_end:
-                
-                while year <= int(final_year):
                     
-                    monthly_fields_0, monthly_fields_1 = None, None
-                    
-                    while (season_start <= month) or (month <= season_end):
+                #After loading data for all months in the season, compute seasonal average
+                compute_seasonal_average([monthly_fields_0, monthly_fields_1], datdir_primary, field_name, season_start_string, season_end_string, '{}-{}'.format(str(year-1), yearstr))
                         
-                        yearstr = str(year)
-                        monthstr = get_monthstr(month)
-                        endmonth = get_month_end(monthstr, yearstr)
-                        date_string = yearstr + "-" + monthstr
-                        StartDate, EndDate = date_string + "-02", date_string + "-" + endmonth
-                        ecco_podaac_download(ShortName=field_shortname, StartDate=StartDate, EndDate=EndDate, download_root_dir=datdir_primary, n_workers=6, force_redownload=False)
-                        ds_month = load_data_files.main(field_name=field_name, date_string=date_string, datdir_primary=datdir_primary, time_ave_type='monthly') 
-
-                        try:
-                            attributes = get_vector_comps(field_name) #For vector fields
-                            if monthly_fields_0 is None:
-                                monthly_fields_0, monthly_fields_1 = ds_month[attributes[0]], ds_month[attributes[1]]
-                            elif monthly_fields_0 is not None:
-                                monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[attributes[0]]], 'time')
-                                monthly_fields_1 = xr.concat([monthly_fields_1, ds_month[attributes[1]]], 'time')
-                        except:
-                            if monthly_fields_0 is None:
-                                monthly_fields_0 = ds_month[get_field_variable(field_name)]
-                            elif monthly_fields_0 is not None:
-                                monthly_fields_0 = xr.concat([monthly_fields_0, ds_month[get_field_variable(field_name)]], 'time')
-                        
-                        if month == 12:
-                            year += 1
-                            month = 1
-                        else:
-                            month += 1
-                    
-                    #After loading data for all months in the season, compute seasonal average
-                    compute_seasonal_average([monthly_fields_0, monthly_fields_1], datdir_primary, field_name, season_start_string, season_end_string, '{}-{}'.format(str(year-1), yearstr))
-                        
-                    month = season_start
+                month = season_start
     
-            print("Done downloading ECCO data and saving seasonal averages.")
+        print("Done downloading ECCO data and saving seasonal averages.")
+        return all_seasons_all_date_strings
             
 ##############################
 
