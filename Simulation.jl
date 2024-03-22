5b0124d4eb535c4f7ecb5bf0462da42bac854af6
R. Cormier <rosalie.cormier@uwaterloo.ca>
2024-03-22 16:07:35 -0400

    Update functions to set realistic initial conditions (GSB)

--- Simulation.jl
+++ Simulation.jl
@@ -5,18 +5,17 @@ using Oceananigans, Oceananigans.Coriolis, Oceananigans.TurbulenceClosures
 SIMULATION PARAMETERS AND GEOMETRY
 =#
 
-Nx, Ny, Nz = 16, 16, 16 #Numbers of gridpoints
-Lx, Ly, Lz = 2π * 100 * 1e3, 2π * 100 * 1e3, 500 #Domain extents
+Nx, Ny, Nz = 32, 32, 32 #Numbers of gridpoints
+Lx, Ly, Lz = 2000 * 1e3, 2000 * 1e3, 1400 #Domain extents
 
-grid = RectilinearGrid(size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz), topology=(Bounded, Bounded, Bounded))
+grid = RectilinearGrid(size=(Nx, Ny, Nz), extent=(Lx, Ly, Lz), topology=(Periodic, Periodic, Bounded))
 
 #=
 DIFFUSIVITIES
 =#
 
 #Constant anisotropic diffusivities
-h_diffusivity, v_diffusivity = 1e-2, 1e-6
-
+h_diffusivity, v_diffusivity = 5e-1, 5e-5 #5e-3, 5e-7
 h_closure = HorizontalScalarDiffusivity(ν=h_diffusivity)
 v_closure = VerticalScalarDiffusivity(ν=v_diffusivity)
 
@@ -44,18 +43,21 @@ model = NonhydrostaticModel(;
 SET INITIAL CONDITIONS
 =#
 
-p0, σr, σz = 1e5, 1e7, 1e3 #Pressure parameters
+σr, σz = 3e5, 4e2 #Standard deviations of pressure in r and z
+p_surf_max = 1.3e5 #Maximum surface pressure
+p0 = p_surf_max / exp(1) # 2π * σr * σz * p_surf_max / exp(1)
 x0, y0 = Lx / 2, Ly / 2 #Centres of horizontal axes
 
 #Function to compute (mean) pressure
-p_bar(x,y,z) = p0 * exp(-((x-x0)/σr)^2) * exp(-((y-y0)/σr)^2) * exp((z/σz)^2)
+p_bar(x,y,z) = p0 * exp(-((x-x0)/σr)^2) * exp(-((y-y0)/σr)^2) * exp(-(z/σz)^2)
     
 grav = 9.80665 #Gravitational acceleration; units m/s^2
+rho_sw = 1020 #Surface seawater density
 
 #Function to compute mean density from mean pressure
-rho_bar(x,y,z) = -2 * p_bar(x,y,z) * z / (grav * (σz^2))
+rho_bar(x,y,z) = rho_sw - (2 * p_bar(x,y,z) * z / (grav * (σz^2)))
 
-N2 = 1 #Squared BV frequency
+N2 = 1e-3 #Squared BV frequency
 
 #Function to compute initial buoyancy profile with perturbation
 function initial_b(x,y,z)
@@ -92,13 +94,13 @@ u, v, w = model.velocities
 b = model.tracers.b
 
 set!(model, u=initial_u, v=initial_v, w=initial_w, b=initial_b)
-print(model.velocities.u.data)
 
 #=
 DEFINE SIMULATION
 =#
 
-simulation = Simulation(model, Δt=10, stop_time=500)
+timestep = 10
+simulation = Simulation(model, Δt=timestep, stop_time=5*60*60)
 
 #=
 SET UP CALLBACK AND OUTPUT WRITER FOR SIMULATION
@@ -145,4 +147,9 @@ open(log_filepath, "w") do file
     write(file, "Horizontal diffusivity = $(h_diffusivity) \n")
     write(file, "Vertical diffusivity = $(v_diffusivity) \n")
     write(file, "f-Plane latitude = $(latitude) degrees N \n")
+    write(file, "Max. surf. p, σr, σz = $(p_surf_max), $(σr), $(σz) \n")
+    write(file, "x0, y0 = $(x0), $(y0) \n")
+    write(file, "g = $(grav) \n")
+    write(file, "N^2 = $(N2) \n")
+    write(file, "Timestep = $(timestep)")
 end
