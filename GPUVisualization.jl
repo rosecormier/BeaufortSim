a96089d33ef99d7c2899aac02c4e7a9f2d917d36
R. Cormier <rosalie.cormier@uwaterloo.ca>
2024-07-18 14:33:52 -0400

    Upload Matheus' visualization code

---
+++ GPUVisualization.jl
@@ -0,0 +1,112 @@
+# Zoom plots for b, v, w
+
+include("Library.jl")
+using Oceananigans
+using NCDatasets, Printf, CairoMakie
+using .VisualizationFunctions
+
+const f     = 0.864e-4
+const fₕ    = 0.0
+const N²    = (3.7e-3)^2
+const ν     = 0.36*1.5/2.2e4
+const Umax  = 0.36*1.5
+const Lx     = 20000
+const Ly     = 30000
+const Lⱼ    = 3000    #1.5x to compensate for Umax.
+const Lz    = 1000
+const D     = 200
+const z0    = -Lz/2
+const y0    = 0 # center of the jet in y
+
+ds = NCDataset("NH_BC_jet_fields.nc", "r")
+x  =   ds["xC"][:]
+y  =   ds["yC"][:]
+z  =   ds["zC"][:]
+Nx = length(ds["xC"][:])
+Ny = length(ds["yC"][:])
+Nz = length(ds["zC"][:])
+times = ds["time"][:]
+
+# imports the parameters directly from the model if running inside the model,
+# assigns a value to them if this script is running by itself.
+
+   Δx = 2*Lx/Nx
+   Δy = 2*Ly/Ny
+   Δz = Lz/Nz
+# pinpoint jet position
+jp_c = (y0 + Ly) ./ Δy
+jp_ini = floor(Int,  jp_c - Lⱼ ./ Δy) - 1125
+jp_end = floor(Int, jp_c + Lⱼ ./ Δy) -125
+
+z_jet = -z0 ./ Δz
+z_jet_ini = floor(Int, z_jet - D/ Δz)
+z_jet_end = floor(Int, z_jet + D/ Δz)
+
+n=Observable(1)
+# get variables
+
+bb = ds["b"][1 , jp_ini : jp_end, z_jet_ini : z_jet_end, 1]
+ub = ds["u"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, 1]
+vb = ds["v"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, 1]
+wb = ds["w"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, 1]
+
+b = @lift ds["b"][1 , jp_ini : jp_end, z_jet_ini : z_jet_end, $n] .- bb
+bt = @lift ds["b"][1 , jp_ini : jp_end, z_jet_ini : z_jet_end, $n]
+u = @lift ds["u"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, $n]
+v = @lift ds["v"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, $n]
+w = @lift ds["w"][: , jp_ini : jp_end, z_jet_ini : z_jet_end, $n]
+
+ωtotal = @lift ζ_2D($u, $v, $w, Δy, Δz)
+∇_b= @lift ∇b_2D($bt, Δy, Δz)
+fq = @lift @. f*($ωtotal + f) .* $∇_b
+
+y  =   y[jp_ini: jp_end]
+z  =   z[z_jet_ini : z_jet_end]
+
+u_yz = @lift $u[1 , :, :]
+v_yz = @lift $v[1 , :, :]
+w_yz = @lift $w[1 , :, :]
+
+# plot data
+
+max_b  =  @lift max(maximum($b), 5e-9)
+lim_b  =  @lift 3/4* [-$max_b,$max_b]
+max_v  =  @lift max(maximum($v_yz), 1e-10)
+lim_v  =  @lift 3/4* [-$max_v,$max_v]
+max_w  =  @lift 1/4* max(maximum($w_yz), 1e-10)
+lim_w  =  @lift 3/4* [-$max_w,$max_w]
+lim_fq = 1 .* [-0.6e-13, 1e-13/5]
+
+
+cm = [Makie.to_colormap(Reverse(:roma))[1:1:128];ones(2,1).*RGBAf(1.0,1.0,1.0,1.0); Makie.to_colormap(Reverse(:roma))[214:254]]
+cm = cm[:,1];
+
+
+fig1=Figure(resolution = (1200, 1200))
+axis_kwargs_yz = (xlabel = "y", ylabel = "z")
+ax_b    = Axis(fig1[2, 1]; title = "b'", axis_kwargs_yz...)
+ax_v    = Axis(fig1[2, 3]; title = "v", axis_kwargs_yz...)
+ax_w    = Axis(fig1[3, 1]; title = "w", axis_kwargs_yz...)
+ax_fq   = Axis(fig1[3, 3]; title = "fq", axis_kwargs_yz...)
+
+hm_b = heatmap!(ax_b, y, z, b, colorrange=lim_b ,colormap = :balance)
+Colorbar(fig1[2, 2], hm_b, tickformat= "{:.1e}")
+hm_v = heatmap!(ax_v, y, z, v_yz, colorrange=lim_v ,colormap = :balance)
+Colorbar(fig1[2, 4], hm_v, tickformat= "{:.1e}")
+hm_w = heatmap!(ax_w, y, z, w_yz, colorrange=lim_w ,colormap = :balance)
+Colorbar(fig1[3, 2], hm_w, tickformat= "{:.1e}")
+hm_fq = heatmap!(ax_fq, y, z, fq, colorrange=lim_fq ,colormap = cm)
+Colorbar(fig1[3, 4], hm_fq, tickformat= "{:.1e}")
+
+title =  @lift @sprintf("t = %.2f days", times[$n]/(3600*24))
+fig1[1, 1:4] = Label(fig1, title, fontsize=24, tellwidth=false)
+frames = 1:length(times)
+video1=VideoStream(fig1,format="mp4", framerate=12)
+for i=1:frames[end]
+    recordframe!(video1)
+    msg = string("Plotting frame ", i, " of ", frames[end])
+        print(msg * " \r")
+        n[]=i
+end
+close(ds)
+save("zoomPlots_fq.mp4", video1)
--- Library.jl
+++ Library.jl
@@ -42,3 +42,188 @@ function ReadInputFile(filename)
     input_params
     
 end
+    
+#= For visualization =#
+    
+using Printf
+#using FileIO, JLD2
+module VisualizationFunctions
+    export buoyancy_C, buoyancy_L, buoyancy_R, u_background, density_from_buoyancy, ωt, ω_b
+    export ζ,ζ_2D, ∇b, ∇b_2D, ertel_q, ertel_q_2D, BestFit, randomSine,randomSineGPU 
+end
+
+function u_background(x,y,z,Umax,D,Lⱼ,z0,y0)
+    Uba = @. Umax/cosh((y-y0)/Lⱼ)^2
+    Ubb = @. exp(-(z-z0)^2/D^2)
+    Ub= Ubb*transpose(Uba) 
+end
+
+function buoyancy_L(x,y,z,N²,f,Umax,D,Lⱼ,z0,y0)
+    Bba = @. 2*f*Umax*Lⱼ/D^2
+    Bbb = @. (tanh((y-y0)/Lⱼ)+1)
+    Bbc = @. (z-z0) * exp(-(z-z0)^2/D^2)
+    b_L = N².*z.+ Bba.* Bbc .* transpose(Bbb)   #Left Boundary = N²*z
+return b_L
+end
+
+function buoyancy_R(x,y,z,N²,f,Umax,D,Lⱼ,z0,y0)
+    Bba = @. 2*f*Umax*Lⱼ/D^2
+    Bbb = @. (tanh((y-y0)/Lⱼ) - 1)
+    Bbc = @. (z-z0) * exp(-(z-z0)^2/D^2)
+    b_R = N².*z .+ Bba.* Bbc .* transpose(Bbb)   #Right Boundary = N²*z
+return b_R
+end
+
+function buoyancy_C(x,y,z,N²,f,Umax,D,Lⱼ,z0,y0)
+    Bba = @. 2*f*Umax*Lⱼ/D^2
+    Bbb = @. (tanh((y-y0)/Lⱼ))
+    Bbc = @. (z-z0) * exp(-(z-z0)^2/D^2)
+    b_C = N².*z.+ Bba.* Bbc .* transpose(Bbb)   #Center = N²*z
+return b_C
+end
+
+function vel_B(x,y,z,N²,f,Umax,D,Lⱼ,z0,y0)
+    Uba = @. Umax/cosh((y-y0)/Lⱼ)^2
+    Ubb = @. exp(-(z-z0)^2/D^2)
+    Ub= Ubb*transpose(Uba)   #background velocity field
+return Ub
+end
+
+function density_from_buoyancy(b, ρ₀)
+    # Compute the density (ρ) from the buoyancy (b) and reference density (ρ₀)
+    ρ = @. ρ₀ * (1 - b / 9.81)
+    return ρ
+end
+
+function ωt(x,y,z,Umax,D,Lⱼ,z0,y0)
+    ωba = @. sech((y-y0)/Lⱼ) * tanh((y-y0)/Lⱼ)
+    ωb = @. 2 * Umax/Lⱼ * exp(-(z-z0)^2/D^2)
+    ωt = ωb .* transpose(ωba)   #background vorticity field
+end
+# function ω_b(x,y,z,Umax,D,Lⱼ,z0)
+#     ωa = @. 2 / Lⱼ * Umax
+#     ωb = @. exp(-(z-z0)^2 / D^2)
+#     ωc = transpose(tanh.(y./Lⱼ) ./(cosh.(y./Lⱼ).^2))
+#     ω_background =  ωa .* ωb .* ωc   #background vorticity field
+# end
+
+function ζ(u, v, w, Δx, Δy, Δz)
+    ζx = (w[2:end,2:end,3:end]-w[2:end,1:end-1,3:end])./Δy .- (v[2:end,3:end,2:end]-v[2:end,3:end,1:end-1])./Δz
+    ζy = (u[2:end,2:end,2:end]-u[2:end,2:end,1:end-1])./Δz - (w[2:end,2:end,3:end]-w[1:end-1,2:end,2:end-1])./Δx
+    ζz = (v[2:end,3:end,2:end]-v[1:end-1,3:end,2:end])./Δx - (u[2:end,2:end,2:end]-u[2:end,1:end-1,2:end])./Δy
+    return ζz #, ζx, ζy, ζz
+end
+
+function ζ_2D(u, v, w, Δy, Δz)
+#    ζx = (w[1,2:end,3:end]-w[1,1:end-1,3:end])./Δy .- (v[1,3:end,2:end]-v[1,3:end,1:end-1])./Δz
+#    ζy = (u[1,2:end,2:end]-u[1,2:end,1:end-1])./Δz
+    ζz = - (u[1,2:end,2:end]-u[1,1:end-1,2:end])./Δy
+    return ζz #, ζx, ζy, ζz
+end
+
+
+function ∇b(b, Δx, Δy, Δz)
+    ∂x_b =  (b[2:end,2:end,2:end]-b[1:end-1,2:end,2:end])/Δx
+    ∂y_b =  (b[2:end,2:end,2:end]-b[2:end,1:end-1,2:end])/Δy
+    ∂z_b =  (b[2:end,2:end,2:end]-b[2:end,2:end,1:end-1])/Δz
+    db_dX = @. ∂x_b + ∂y_b + ∂z_b
+    return ∂z_b
+end
+
+function ∇b_2D(b, Δy, Δz)
+    ∂x_b =  0.0
+    ∂y_b =  (b[2:end,2:end]-b[1:end-1,2:end])/Δy
+    ∂z_b =  (b[2:end,2:end]-b[2:end,1:end-1])/Δz
+    db_dX = @. ∂x_b + ∂y_b + ∂z_b
+    return db_dX#, ∂x_b, ∂y_b, ∂z_b
+end
+
+function ertel_q(u, v, w, b, Δx, Δy, Δz)
+    ζt,ζx,ζy,ζz = ζ(u, v, w, Δx, Δy, Δz)
+    ∇b, ∂x_b, ∂y_b, ∂z_b = ∇b(b, Δx, Δy, Δz)
+    qvert = (f .+ ζz).* ∂z_b
+    q_bc = @. ζx * ∂x_b + ζy * ∂y_b
+    qt = @. q_bc .+ qvert
+    return qt, qvert, q_bc
+end
+
+function ertel_q_2D(u, v, w, b, Δy, Δz)
+    ζt, ζx, ζy, ζz = ζ(u, v, w, Δy, Δz)
+    db_dX, ∂x_b, ∂y_b, ∂z_b = ∇b(b, Δy, Δz)
+    qvert = (f .+ ζz).*∂z_b
+    q_bc = @. ζx * ∂x_b + ζy * ∂y_b
+    qt = @. q_bc .+ qvert
+    return qt, qvert, q_bc
+end
+
+function BestFit(degree,interval, abscissa,ordenate)
+    linear_fit_polynomial = fit(abscissa[interval], log.(ordenate[interval]), degree, var = :abscissa)
+    constant, slope = linear_fit_polynomial[0], linear_fit_polynomial[1]
+    best_fit = @. exp(constant + slope * abscissa)
+    @sprintf "The growth rate is approximately %5.1e" slope
+    return best_fit, constant, slope
+end
+#=
+function randomSine(x,y,z, Nx,Ny,Nz)
+#Random coefficients (gaussian-distributed) for phase (ϕ) and amplitude (A)
+
+amplitude = FileIO.load("cosineVariables.jld2","amplitude")
+phase = FileIO.load("cosineVariables.jld2","phase")
+
+ϕx = phase[:,1]
+Ax = amplitude[:,1]
+ϕy = phase[:,2]
+Ay = amplitude[:,2]
+ϕz = phase[:,3]
+Az = amplitude[:,3]
+
+rs = 0
+    for k = 1:400
+        rs = rs *(Ax[k].*sin.(2*π .* x .* (k) / Nx .+ ϕx[k]) .* Ay[k].*sin.(2*π.*y .* (k) / Ny .+ ϕy[k]) .* 
+        Az[k].*sin.(2*π.*z .* (k) / Nz .+ ϕz[k]))
+    end
+    return rs
+end
+
+function randomSineGPU(x,y,z, Nx,Ny,Nz)
+#Random coefficients (gaussian-distributed) for phase (ϕ) and amplitude (A)
+
+amplitude = FileIO.load("cosineVariables.jld2","amplitude")
+phase = FileIO.load("cosineVariables.jld2","phase")
+
+ϕx = phase[:,1]
+Ax = amplitude[:,1]
+ϕy = phase[:,2]
+Ay = amplitude[:,2]
+ϕz = phase[:,3]
+Az = amplitude[:,3]
+
+rs = 0
+Threads.@threads for k = 1:400
+        rs = rs + (Ax[k].*sin.(2*π .* x .* (k) / Nx .+ ϕx[k]) .* Ay[k].*sin.(2*π.*y .* (k) / Ny .+ ϕy[k]) .*
+        Az[k].*sin.(2*π.*z .* (k) / Nz .+ ϕz[k]))
+    end
+    return rs
+end
+
+function randomSineGPU_2D(y,z, Ny,Nz)
+#Random coefficients (gaussian-distributed) for phase (ϕ) and amplitude (A)
+
+amplitude = FileIO.load("cosineVariables.jld2","amplitude")
+phase = FileIO.load("cosineVariables.jld2","phase")
+
+#ϕx = phase[:,1]
+#Ax = amplitude[:,1]
+ϕy = phase[:,2]
+Ay = amplitude[:,2]
+ϕz = phase[:,3]
+Az = amplitude[:,3]
+
+rs = 0
+Threads.@threads for k = 1:400
+        rs = rs + 1/400 .*(Ay[k].*sin.(2*π.*y .* (k) / Ny .+ ϕy[k]) .*
+        Az[k].*sin.(2*π.*z .* (k) / Nz .+ ϕz[k]))
+    end
+    return rs
+end
+=#
