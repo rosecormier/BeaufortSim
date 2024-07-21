4b456d9ae6668846f92f7abc8b44a14d8cfafb51
Rosalie Cormier <rmc2corm@pelee.math.private.uwaterloo.ca>
2024-07-21 15:43:31 -0400

    Save animations with descriptive filenames

--- GPUVisualization.jl
+++ GPUVisualization.jl
@@ -26,6 +26,7 @@ const depth_idx = parse(Int, ARGS[2])
 x  =   ds["xC"][:]
 y  =   ds["yC"][:]
 z  =   ds["zC"][depth_idx]
+depth_nearest_m = round(Int, -z)
 
 Nx = length(ds["xC"][:])
 Ny = length(ds["yC"][:])
@@ -65,6 +66,7 @@ max_v  = @lift max(maximum($v_xy), 1e-10)
 lim_v  =  @lift 3/4* [-$max_v,$max_v]
 max_w  = @lift 1/4* max(maximum($w_xy), 1e-10)
 lim_w  =  @lift 3/4* [-$max_w,$max_w]
+
 lim_fq = 1 .* [-0.6e-13, 1e-13/5]
 
 cm = [Makie.to_colormap(Reverse(:roma))[1:1:128];ones(2,1).*RGBAf(1.0,1.0,1.0,1.0); Makie.to_colormap(Reverse(:roma))[214:254]]
@@ -73,23 +75,23 @@ cm = cm[:,1];
 fig1=Figure(size = (1200, 1200))
 axis_kwargs_xy = (xlabel = "x", ylabel = "y")
 ax_b    = Axis(fig1[2, 1]; title = "b'", axis_kwargs_xy...)
-ax_u    = Axis(fig1[2, 3]; title = "u", axis_kwargs_xy...)
-ax_v    = Axis(fig1[3, 1]; title = "v", axis_kwargs_xy...)
-ax_w    = Axis(fig1[3, 3]; title = "w", axis_kwargs_xy...)
+ax_w    = Axis(fig1[2, 3]; title = "w", axis_kwargs_xy...)
+ax_u    = Axis(fig1[3, 1]; title = "u", axis_kwargs_xy...)
+ax_v    = Axis(fig1[3, 3]; title = "v", axis_kwargs_xy...)
 #ax_fq   = Axis(fig1[3, 3]; title = "fq", axis_kwargs_xy...)
 
-hm_b = heatmap!(ax_b, x, y, b, colorrange=lim_b, colormap = :balance)
-Colorbar(fig1[2, 2], hm_b, tickformat= "{:.1e}")
-hm_u = heatmap!(ax_u, x, y, u_xy, colorrange=lim_v, colormap = :balance)
-Colorbar(fig1[2, 4], hm_u, tickformat= "{:.1e}")
-hm_v = heatmap!(ax_v, x, y, v_xy, colorrange=lim_v, colormap = :balance)
-Colorbar(fig1[3, 2], hm_v, tickformat= "{:.1e}")
-hm_w = heatmap!(ax_w, x, y, w_xy, colorrange=lim_w, colormap = :balance)
-Colorbar(fig1[3, 4], hm_w, tickformat= "{:.1e}") #2], hm_w, tickformat= "{:.1e}")
+hm_b = heatmap!(ax_b, x, y, b, colorrange = lim_b, colormap = :balance)
+Colorbar(fig1[2, 2], hm_b, tickformat = "{:.1e}")
+hm_w = heatmap!(ax_w, x, y, w_xy, colorrange = lim_w, colormap = :balance)
+Colorbar(fig1[2, 4], hm_w, tickformat = "{:.1e}")
+hm_u = heatmap!(ax_u, x, y, u_xy, colorrange = lim_v, colormap = :balance)
+Colorbar(fig1[3, 2], hm_u, tickformat = "{:.1e}")
+hm_v = heatmap!(ax_v, x, y, v_xy, colorrange = lim_v, colormap = :balance)
+Colorbar(fig1[3, 4], hm_v, tickformat = "{:.1e}") #2], hm_w, tickformat= "{:.1e}")
 #hm_fq = heatmap!(ax_fq, y, z, fq, colorrange=lim_fq ,colormap = cm)
 #Colorbar(fig1[3, 4], hm_fq, tickformat= "{:.1e}")
 
-title =  @lift @sprintf("t = %.2f days", times[$n]/(3600*24))
+title = @lift @sprintf("t = %.2f days", times[$n]/(3600*24))
 fig1[1, 1:4] = Label(fig1, title, fontsize=24, tellwidth=false)
 frames = 1:length(times)
 video1=VideoStream(fig1,format="mp4", framerate=12)
@@ -99,5 +101,6 @@ for i=1:frames[end]
         print(msg * " \r")
         n[]=i
 end
+
+save("bwuv_z$(depth_nearest_m)_$(datetime).mp4", video1)
 close(ds)
-save("zoomPlots_fq.mp4", video1)
