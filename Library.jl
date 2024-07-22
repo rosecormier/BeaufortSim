6d6899dd6c3b4d4d3d536e45b42ead44af5701a7
Rosalie Cormier <rmc2corm@pelee.math.private.uwaterloo.ca>
2024-07-22 15:12:19 -0400

    Fix spacing

--- Library.jl
+++ Library.jl
@@ -1,12 +1,8 @@
 function ReadInputFile(filename)
-    
     input_params = Dict()
-    
     for (i, line) in enumerate(eachline(filename))
-        
         var_len, line_len = 0, length(line)
         if (line_len > 1 && line[1] != '#')
-
             for char in line
                 if char == '='
                     break
@@ -14,10 +10,8 @@ function ReadInputFile(filename)
                     var_len += 1
                 end
             end
-
             var = strip(line[1:var_len])
             line = strip(line[var_len:end])
-
             val_len = 0
             for char in line
                 if char == '#' 
@@ -26,21 +20,15 @@ function ReadInputFile(filename)
                     val_len += 1
                 end
             end
-
             val_str = strip(line[2:val_len]) #Index 1 is "="
-
             val = tryparse(Int, val_str)
             if isnothing(val)
                 val = parse(Float64, val_str)
             end
-
             input_params[var] = val
-
         end    
     end
-    
     input_params
-    
 end
     
 #= For visualization =#
