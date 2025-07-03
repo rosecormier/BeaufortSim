e9f2ce08b2b71d53e5a6c2b57b276b9afbfac0a0
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-07-03 19:13:19 -0400

    Remove duplicate lines

--- LinearStability/BuildBkgdOperators.py
+++ LinearStability/BuildBkgdOperators.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.sparse as sp
 
+from math import e
+
 def rInterior(params, geom, discretize2D = False):
     """
     Build array of those r-values at interior gridpoints that are physical.
@@ -26,36 +28,29 @@ def BuildBkgdOperators(params, geom, discretize2D = False):
      evaluated at gridpoints.
     """
 
-    #halfNr, Np       = params.halfNr, params.Np
-    #[rInterior, phi] = np.meshgrid(geom.r[1:(halfNr + 1)],
-    #                               np.arange(1, (Np + 1)))
-
-    #Array of those r-values at interior gridpoints that lie in physical space
-    #rInterior = np.hstack(np.stack(rInterior[:], axis = -1))
-
     rInterior = geom.rInterior
 
     if params.bkgd == "GM":
         Ψ_op = np.ravel(-0.5 * np.exp(-rInterior**2))
         Q_op = np.ravel(-2 * np.exp(-rInterior**2) * (rInterior**2 - 2))
 
-        if sp.issparse(geom.Dr):
-            
-            if discretize2D:
-                opLength = params.halfNr * params.Np
-            else:
-                opLength = params.halfNr
-    
-            Ψ_op = sp.spdiags(Ψ_op, 0, opLength, opLength)
-            Q_op = sp.spdiags(Q_op, 0, opLength, opLength)
-
-        else:
-            Ψ_op, Q_op = np.diag(Ψ_op), np.diag(Q_op)
-
     elif params.bkgd == "BG":
         Ψ_op = np.ravel(np.sqrt(2*e) * np.exp(-(rInterior**2)))
         Q_op = np.ravel(np.sqrt(32*e) * (rInterior**2 - 2)
                         * np.exp(-rInterior**2))
+
+    if sp.issparse(geom.Dr):
+            
+        if discretize2D:
+            opLength = params.halfNr * params.Np
+        else:
+            opLength = params.halfNr
+    
+        Ψ_op = sp.spdiags(Ψ_op, 0, opLength, opLength)
+        Q_op = sp.spdiags(Q_op, 0, opLength, opLength)
+
+    else:
+        Ψ_op, Q_op = np.diag(Ψ_op), np.diag(Q_op)
 
     return Ψ_op, Q_op
 
