4235a71b8e182f232e9009e8664538268a03e59b
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-07-28 09:33:01 -0400

    Plot formatting

--- PythonVisualization.py
+++ PythonVisualization.py
@@ -1,10 +1,13 @@
 from glob import glob
 from netCDF4 import Dataset
 from numpy.linalg import norm
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-bkgd_file = r"./Output/bkgd_250706-143810.nc"
+datetime = "250706-143810"
+
+bkgd_file = fr"./Output/bkgd_{datetime}.nc"
 openbkgd  = Dataset(bkgd_file, "r")
 uφ_bkgd   = openbkgd.variables["Uφ"][0, :, :, :]
 
@@ -14,13 +17,13 @@ all_times      = []
 ur_norms_log   = []
 uphi_norms_log = []
 
-file_list = glob(r"./Output/output_250706-143810*.nc")
+file_list = glob(fr"./Output/output_{datetime}*.nc")
 
 for filepath in file_list:
 
     openfile = Dataset(filepath, "r")
   
-    times = (openfile.variables["time"][:]) / 86400 #Convert to days
+    times = (openfile.variables["time"][:])
     ur    = openfile.variables["ur"][:, :, :, :]
     uphi  = openfile.variables["uφ"][:, :, :, :]
 
@@ -40,9 +43,11 @@ uphi_norms_log = np.array(uphi_norms_log)
 
 time_indexing = np.argsort(all_times)
 
-times              = (all_times[time_indexing])[60:-160]
-ur_norms_log_fit   = (ur_norms_log[time_indexing])[60:-160]
-uphi_norms_log_fit = (uphi_norms_log[time_indexing])[60:-160]
+fit_idx_start, fit_idx_stop = 55, 140
+
+times              = (all_times[time_indexing])[fit_idx_start:fit_idx_stop]
+ur_norms_log_fit   = (ur_norms_log[time_indexing])[fit_idx_start:fit_idx_stop]
+uphi_norms_log_fit = (uphi_norms_log[time_indexing])[fit_idx_start:fit_idx_stop]
 
 ur_fit_params   = np.polyfit(times, ur_norms_log_fit, 1)
 uphi_fit_params = np.polyfit(times, uphi_norms_log_fit, 1)
@@ -51,11 +56,20 @@ print("Best-fit parameters for log|u_r|: [slope, intercept] = ", ur_fit_params, 
 print("Best-fit parameters for log|u_phi|: [slope, intercept] = ", uphi_fit_params, "\n")
 
 fig, ax = plt.subplots(1, 1)
-ax.scatter(all_times, ur_norms_log, color = "red", label = "$||u_r'||$")
-ax.scatter(all_times, uphi_norms_log, color = "orange", label = "$||u_{{\phi}}'||$")
-ax.plot(np.array(times), ur_fit_params[0] * np.array(times) + ur_fit_params[1], color = "red", label = "Best fit to $||u_r'||$")
-ax.plot(np.array(times), uphi_fit_params[0] * np.array(times) + uphi_fit_params[1], color = "orange", label = "Best fit to $||u_{{\phi}}'||$")
-ax.set(xlabel = "Time [days]", ylabel = "Log of field norm")
+ax.scatter(all_times / 86400, ur_norms_log, color = "red", 
+           label = "$||u_r'||$")
+ax.scatter(all_times / 86400, uphi_norms_log, color = "orange", 
+           label = "$||u_{{\phi}}'||$")
+ax.plot(np.array(times) / 86400, 
+        ur_fit_params[0] * np.array(times) / 86400 + ur_fit_params[1], 
+        color = "mediumblue", label = "Best fit to $||u_r'||$")
+ax.plot(np.array(times) / 86400, 
+        uphi_fit_params[0] * np.array(times) / 86400 + uphi_fit_params[1], 
+        color = "mediumpurple", label = "Best fit to $||u_{{\phi}}'||$")
+ax.set(xlabel = "Time [days]", ylabel = "Log of $\ell^2$-norm [m/s]")
+ax.legend()
 plt.show()
-fig.savefig("test.png")
+fig.savefig(f"norms_{datetime}_fit_{fit_idx_start}-{fit_idx_stop}.png")
 plt.close(fig)
+
+t_idx = 80
