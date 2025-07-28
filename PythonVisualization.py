c9af23b74dd2ec3467e1b6f7f6f3dc118052a2b2
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-07-28 19:46:36 -0400

    In progress: plot spatial structures of perturbation fields

--- PythonVisualization.py
+++ PythonVisualization.py
@@ -19,18 +19,26 @@ ur_norms_log   = []
 uphi_norms_log = []
 uz_norms_log   = []
 
+ur_data   = []
+uphi_data = []
+uz_data   = []
+
 file_list = glob(fr"./Output/output_{datetime}*.nc")
 
 for filepath in file_list:
 
     openfile = Dataset(filepath, "r")
-  
-    times = (openfile.variables["time"][:])
+     
+    x     = openfile.variables["xC"][:]
+    y     = openfile.variables["yC"][:]
+    z     = openfile.variables["zC"][:]
+    times = openfile.variables["time"][:]
     ur    = openfile.variables["ur"][:, :, :, :]
     uphi  = openfile.variables["uφ"][:, :, :, :]
     uz    = openfile.variables["uz"][:, :, :, :]
 
     for t in range(len(times)):
+    
        ur_norm_t   = norm(ur[t, :, :, :])
        uphi_norm_t = norm(uphi[t, :, :, :] - uφ_bkgd)
        uz_norm_t   = norm(uz[t, :, :, :])
@@ -40,12 +48,21 @@ for filepath in file_list:
        uphi_norms_log.append(np.log(uphi_norm_t))
        uz_norms_log.append(np.log(uz_norm_t))
 
+       ur_data.append(ur[t, :, :, :])
+       uphi_data.append(uphi[t, :, :, :])
+       uz_data.append(uz[t, :, :, :])
+
     openfile.close()
 
 all_times      = np.array(all_times)
+
 ur_norms_log   = np.array(ur_norms_log)
 uphi_norms_log = np.array(uphi_norms_log)
 uz_norms_log   = np.array(uz_norms_log)
+
+ur_data   = np.array(ur_data)
+uphi_data = np.array(uphi_data)
+uz_data   = np.array(uz_data)
 
 time_indexing = np.argsort(all_times)
 
@@ -83,9 +100,37 @@ ax.plot(np.array(times) / 86400,
 ax.set(xlabel = "Time [days]", ylabel = "Log of $\ell^2$-norm [m/s]")
 ax.legend()
 plt.grid(True)
-plt.show()
+#plt.show()
 fig.savefig(join("Plots",
             f"norms_{datetime}_fit_{fit_idx_start}-{fit_idx_stop}.png"))
 plt.close(fig)
 
-t_idx = 80
+ur_data   = ur_data[time_indexing]
+uphi_data = uphi_data[time_indexing]
+uz_data   = uz_data[time_indexing]
+
+for k in range(3, len(z)-3, 1):
+
+    for t in range(fit_idx_start, fit_idx_stop, 3):
+        print(t, "\n")
+        time = all_times[t]
+        zval = z[k]
+
+        fig, axs = plt.subplots(1, 2, sharey = True)
+        pcm_ur = axs[0].pcolormesh(x[200:606] / 1e3, y[200:606] / 1e3,
+                                     ur_data[t, k, 200:606, 200:606],
+                                     cmap = "seismic")
+        axs[0].set(xlabel = "x [km]", ylabel = "y [km]")
+        plt.colorbar(pcm_ur, ax = axs[0])
+        pcm_uphi = axs[1].pcolormesh(x[200:606] / 1e3, y[200:606] / 1e3,
+                            (uphi_data[t, k, 200:606, 200:606]
+                             - uφ_bkgd[k, 200:606, 200:606]),
+                            cmap = "seismic")
+        axs[1].set(xlabel = "x [km]", ylabel = "y [km]")
+        plt.colorbar(pcm_uphi, ax = axs[1])
+        fig.suptitle(f"Perturbations in horizontal velocity components; \n t = {time/86400:.2f} days, z = {zval} m")
+        #plt.show()
+        fig.savefig(join("Plots",
+                    f"ur_uphi_perturb_{datetime}_t{t}_z{zval}.png"))
+        plt.close(fig)
+
