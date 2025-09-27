6759d0562b44ed6e88eb94b542bf548b47431354
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-09-27 14:01:24 -0400

    MWE for testing halo cells

--- MWE.jl
+++ MWE.jl
@@ -1,137 +1,38 @@
-include("Visualization.jl")
-
-using Adapt, CUDA
-using Dates: canonicalize, format, now
-using NCDatasets
 using Oceananigans
+using Oceananigans.Advection
 using Oceananigans.Architectures
 using Oceananigans.BoundaryConditions
-using Oceananigans.Coriolis
-using Oceananigans.Fields
-using Oceananigans.Operators
 using Oceananigans.Units
-using Oceananigans.Utils
-using Oceanostics
-using Printf, Random
+using Printf
 
-#Numbers of gridpoints
-const Nx = 12
-const Ny = 12
-const Nz = 12
+grid = RectilinearGrid(CPU(), topology = (Flat, Flat, Bounded), size = (8), z = (-1000 * meter, 0))
 
-#Lengths of axes
-const Lx = 2.5e3 * kilometer
-const Ly = 2.5e3 * kilometer
-const Lz = 1 * kilometer
+N²       = 3e-3 * (second^(-2))
+b̄z_BC(t) = N²
+b̄(z)     = N² * z
+b̄_BCs    = FieldBoundaryConditions(top = GradientBoundaryCondition(b̄z_BC),
+                                bottom = GradientBoundaryCondition(b̄z_BC))
 
-#Latitude (deg. N)
-const lat = 74.0
 
-#f-plane and Coriolis frequency
-fPlane  = FPlane(latitude = lat)
-const f = fPlane.f
-
-#Characteristic scales for basic state
-const σr  = 250 * kilometer
-const U   = 1.5e-1 * (meter/second)
-const N²₀ = 3e-3 * (second^(-2))
-
-#Time-stepping parameters
-const Δt      = 10 * second
-const tf      = 3 * hour
-const Δt_save = 1 * hour
-
-#Max. relative magnitude of initial u-perturbations
-const max_u′ = 1e-8
-
-const use_GPU = false
-
-use_GPU ? architecture = GPU() : architecture = CPU()
-
-grid = RectilinearGrid(architecture,
-		       topology = (Bounded, Bounded, Bounded),
-                       size = (Nx, Ny, Nz), 
-                       x = (-Lx/2, Lx/2), 
-                       y = (-Ly/2, Ly/2), 
-                       z = (-Lz, 0.0),
-		       halo = (3, 3, 3))
-
-b̄ = (x, y, z) -> N²₀ * z
-ū = (x, y, z) -> (sqrt(2)*U*y/σr) * exp((1/2) - (x^2 + y^2)/(σr^2))
-v̄ = (x, y, z) -> -(sqrt(2)*U*x/σr) * exp((1/2) - (x^2 + y^2)/(σr^2))
-
-b̄z_top = (x, y, t) -> N²₀
-b̄z_bot = (x, y, t) -> N²₀
-b̄_BCs  = FieldBoundaryConditions(top    = GradientBoundaryCondition(b̄z_top),
-         			 bottom = GradientBoundaryCondition(b̄z_bot))
-
-model = NonhydrostaticModel(; 
-                            grid = grid, 
+model = NonhydrostaticModel(;
+                            grid = grid,
                             timestepper = :RungeKutta3,
-                            advection = UpwindBiasedFifthOrder(), 
-                            coriolis = fPlane,
+                            advection = WENO(),
                             tracers = (:b),
                             buoyancy = BuoyancyTracer(),
-			    boundary_conditions = (; b = b̄_BCs,))
+                            boundary_conditions = (; b = b̄_BCs))
 
-set!(model, u = ū, v = v̄)
-fill_halo_regions!(model.velocities)
 set!(model, b = b̄)
 
-datetimenow = format(now(), "yymmdd-HHMMSS")
+simulation = Simulation(model, Δt = 2.5 * second, stop_time = 2.5 * second)
 
-Ux = XFaceField(model.grid)
-Uy = YFaceField(model.grid)
-Uz = ZFaceField(model.grid)
-
-set!(Ux, model.velocities.u)
-set!(Uy, model.velocities.v)
-set!(Uz, model.velocities.w)
-fill_halo_regions!(Ux)
-fill_halo_regions!(Uy)
-fill_halo_regions!(Uz)
-
-@inline ψ′²(i, j, k, grid, ψ, ψ̄) = (ψ[i, j, k] - ψ̄[i, j, k])^2 #from TurbulentKineticEnergyEquation
-
-@inline pKE_ccc(i, j, k, grid, u, v, w, Ux, Uy, Uz) = (
-                              ℑxᶜᵃᵃ(i, j, k, grid, ψ′², u, Ux) +
-                              ℑyᵃᶜᵃ(i, j, k, grid, ψ′², v, Uy) +
-                              ℑzᵃᵃᶜ(i, j, k, grid, ψ′², w, Uz)
-                             ) / 2
-
-pKE_op = KernelFunctionOperation{Center, Center, Center}(pKE_ccc,
-							 grid, 
-							 model.velocities.u, 
-							 model.velocities.v, 
-							 model.velocities.w,
-							 Ux, Uy, Uz)
-
-function compute_pKE(sim)
-   compute!(pKE_op)
+function progress(sim)
+   @info @sprintf("Iteration %d; time = %.2e days",
+                  iteration(sim), (time(sim)/day))
+   print("Buoyancy slice: ", sim.model.tracers.b[1, 1, :], "\n")
+   return nothing
 end
 
-#Perturb velocity components to trigger instability
-@inline u_perturbed(x, y, z) = (ū(x, y, z) + (2 * (rand() - 0.5)) * (max_u′ / sqrt(2)))
-@inline v_perturbed(x, y, z) = (v̄(x, y, z) + (2 * (rand() - 0.5)) * (max_u′ / sqrt(2)))
-set!(model, u = u_perturbed, v = v_perturbed)
-fill_halo_regions!(model.velocities, model.tracers.b)
-
-simulation = Simulation(model, Δt = Δt, stop_time = tf)
-
-energy_diagnostics = (; pKE = compute_pKE(simulation))
-
-energyfilepath = joinpath("./Output", "energetics_$(datetimenow).nc")
-mkpath(dirname(energyfilepath)) #Make path if nonexistent
-
-energy_writer = NetCDFOutputWriter(model, 
-				   energy_diagnostics,
-				   with_halos = true,
-				   filename = energyfilepath,
-				   schedule = TimeInterval(Δt_save),
-				   file_splitting = FileSizeLimit(30GiB))
-
-simulation.output_writers[:energy_writer] = energy_writer
+add_callback!(simulation, progress, IterationInterval(1))
 
 run!(simulation)
-
-visualize_energetics(datetimenow, grid)
