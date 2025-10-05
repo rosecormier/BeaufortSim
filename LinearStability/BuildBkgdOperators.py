df1cc63d62166ce55ee449b03d549ca78e64d865
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-10-05 18:23:27 -0400

    Change imports to improve readability; start discretization in z

--- LinearStability/BuildBkgdOperators.py
+++ LinearStability/BuildBkgdOperators.py
@@ -1,7 +1,6 @@
-import numpy as np
 import scipy.sparse as sp
 
-from math import e
+from numpy import arange, diag, exp, hstack, meshgrid, ravel, sqrt, stack
 
 def rInterior(params, geom, discretizeAzim = False):
     """
@@ -10,17 +9,18 @@ def rInterior(params, geom, discretizeAzim = False):
 
     if discretizeAzim:
         halfNr, Np       = params.halfNr, params.Np
-        [rInterior, phi] = np.meshgrid(geom.r[1:(halfNr + 1)],
-                                       np.arange(1, (Np + 1)))
-        rInterior        = np.hstack(np.stack(rInterior[:], axis = -1))
+        [rInterior, phi] = meshgrid(geom.r[1:(halfNr + 1)], arange(1, (Np + 1)))
+        rInterior        = hstack(stack(rInterior[:], axis = -1))
 
     else:
         halfNr    = params.halfNr
-        rInterior = np.ravel(geom.r[1:(halfNr + 1)])
+        rInterior = ravel(geom.r[1:(halfNr + 1)])
 
     return rInterior
 
-def BuildBkgdOperators(params, geom, discretizeAzim = False, 
+def BuildBkgdOperators(params, geom, 
+                       discretizeAzimuth = False, 
+                       discretizeVertical = False,
                        dimensional_U = 1, dimensional_σr = 1):
     """
     Build array of (1/r) * (dPsi/dr) and array of (1/r) * (dQ/dr), each
@@ -30,22 +30,40 @@ def BuildBkgdOperators(params, geom, discretizeAzim = False,
     rInterior = geom.rInterior
     
     if params.bkgd == "GM":
-        Ψ_op = np.ravel((-0.5 * np.exp(-rInterior**2 / dimensional_σr**2)) 
-                        * (dimensional_U / dimensional_σr))
-        Q_op = np.ravel((-2 * np.exp(-rInterior**2 / dimensional_σr**2) 
-                        * ((rInterior / dimensional_σr)**2 - 2)) 
-                        * (dimensional_U / dimensional_σr**3))
+        Ψ_op = ravel(-0.5
+                     * exp(-rInterior**2 / dimensional_σr**2)
+                     * (dimensional_U / dimensional_σr))
+        Q_op = ravel(-2
+                     * exp(-rInterior**2 / dimensional_σr**2) 
+                     * ((rInterior / dimensional_σr)**2 - 2)
+                     * (dimensional_U / dimensional_σr**3))
 
     elif params.bkgd == "BG":
-        Ψ_op = np.ravel((np.sqrt(2*e) * np.exp(-(rInterior**2 / dimensional_σr**2))) 
-                        * (dimensional_U / dimensional_σr))
-        Q_op = np.ravel((np.sqrt(32*e) * ((rInterior / dimensional_σr)**2 - 2)
-                        * np.exp(-rInterior**2 / dimensional_σr**2))
-                        * (dimensional_U / dimensional_σr**3))
+
+        if discretizeVertical: #Discretize in both r and z
+            Ψ_op = ravel(sqrt(2) 
+                         * exp(0.5 - (rInterior**2 / dimensional_σr**2)
+                               - (zInterior**2 / dimensional_σz**2))
+                         * (dimensional_U / dimensional_σr))
+            Q_op = ravel(sqrt(8) 
+                         * ((2 * (rInterior / dimensional_σr)**2 - 2)
+                            (1 / Bu) * ())
+                         * exp(0.5 - (rInterior**2 / dimensional_σr**2)
+                               - (zInterior**2 / dimensional_σz**2))
+                         * (dimensional_U / dimensional_σr**3))
+        
+        else: #Discretize in r only
+            Ψ_op = ravel(sqrt(2) 
+                         * exp(0.5 - (rInterior**2 / dimensional_σr**2))
+                         * (dimensional_U / dimensional_σr))
+            Q_op = ravel(sqrt(32) 
+                         * ((rInterior / dimensional_σr)**2 - 2)
+                         * exp(0.5 - rInterior**2 / dimensional_σr**2)
+                         * (dimensional_U / dimensional_σr**3))
 
     if sp.issparse(geom.Dr):
             
-        if discretizeAzim:
+        if discretizeAzimuth:
             opLength = params.halfNr * params.Np
         else:
             opLength = params.halfNr
@@ -54,6 +72,6 @@ def BuildBkgdOperators(params, geom, discretizeAzim = False,
         Q_op = sp.spdiags(Q_op, 0, opLength, opLength)
 
     else:
-        Ψ_op, Q_op = np.diag(Ψ_op), np.diag(Q_op)
+        Ψ_op, Q_op = diag(Ψ_op), diag(Q_op)
 
     return Ψ_op, Q_op
