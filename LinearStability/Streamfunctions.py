482375374ea576884e6c45c4ffffdf10bab5e00b
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-10-20 09:57:51 -0400

    Compute eigenvelocities from eigenfunction

--- LinearStability/Streamfunctions.py
+++ LinearStability/Streamfunctions.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 from math import cos, sin
 
-def GetStreamfunc(eigenvec, **kwargs):
+def GetStreamfunc(eigvec, **kwargs):
     """
     Evaluates streamfunction at a specified (discrete) eigenvector and any of
      phi, z, and t, where k, m, and omega, respectively, must be provided.
@@ -9,7 +11,7 @@ def GetStreamfunc(eigenvec, **kwargs):
     k, m, omega = kwargs.get("k"), kwargs.get("m"), kwargs.get("omega")
     phi, z, t   = kwargs.get("phi"), kwargs.get("z"), kwargs.get("t")
 
-    psi = eigenvec
+    psi = eigvec
 
     if k is not None:
         psi = psi * (cos(k*phi) + 1j * sin(k*phi))
@@ -22,10 +24,33 @@ def GetStreamfunc(eigenvec, **kwargs):
 
     return psi
 
-#def VelocityFromStreamfunc(psi, ):
+def EigenvelocityFromEigvec(params, geom, eigvec, k, **kwargs):
+    """
+    Evaluates QG velocities corresponding to specified (discrete) eigenvector
+     and azimuthal wavenumber, as well as any of φ, z, and t, (m and 
+     omega, respectively, must be provided for the latter two).
+    """
 
-    
+    halfNr = params.halfNr
+    r      = geom.r[1:(halfNr + 1)]
+    Dr     = geom.Dr[1:(halfNr + 1), 1:(halfNr + 1)]
 
-#    ux = 
+    m, omega = kwargs.get("m"), kwargs.get("omega")
+    φ, z, t  = kwargs.get("φ"), kwargs.get("z"), kwargs.get("t")
 
-#    return ux, uy
+    ur = np.dot((1j * k / r), eigvec)
+    uφ = -np.dot(Dr, eigvec)
+
+    if φ is not None:
+        ur = ur * (cos(k*φ) + 1j * sin(k*φ))
+        uφ = uφ * (cos(k*φ) + 1j * sin(k*φ))
+
+    if m is not None:
+        ur = ur * (cos(m*z) + 1j * sin(m*z))
+        uφ = uφ * (cos(m*z) + 1j * sin(m*z))
+
+    if omega is not None:
+        ur = ur * (cos(omega*t) - 1j * sin(omega*t))
+        uφ = uφ * (cos(omega*t) - 1j * sin(omega*t))
+
+    return ur, uφ
