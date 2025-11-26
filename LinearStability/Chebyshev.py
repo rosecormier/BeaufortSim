d198087694149bad5828b579fc60d2d8f19c6bed
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2025-11-26 13:55:36 -0500

    Fix formatting

--- LinearStability/Chebyshev.py
+++ LinearStability/Chebyshev.py
@@ -26,16 +26,16 @@ def Chebyshev(N):
         x = np.cos(pi * np.arange(0, (N+1)) / N)
 
         #Define the Chebyshev coeffs c_{ij}
-        c = np.hstack([2, np.ones(N - 1), 2]) * (-1)**np.arange(0, (N+1))
+        c = np.hstack([2, np.ones(N-1), 2]) * (-1)**np.arange(0, (N+1))
         
-        X = np.tile(x, (N + 1, 1))
+        X  = np.tile(x, (N+1, 1))
         dX = X.T - X #Element-wise difference between X^T and X
 
         #Initialize D with off-diag entries computed by eq. 6.5 (Trefethen)
         D = ((c[:, np.newaxis] * (1.0 / c)[np.newaxis, :])
-             / (dX + (np.identity(N + 1))))
+             / (dX + (np.identity(N+1))))
 
         #Update diagonal entries (currently = 0) using identity 6.6 (Trefethen)
-        D = D - np.diag(D.sum(axis=1))
+        D = D - np.diag(D.sum(axis = 1))
 
     return D, x
