91ca1ee6f9d851432e93976f9cff5d09bfa296de
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-01-12 15:32:23 -0500

    Fix Kronecker products

--- LinearStability/BuildBkgdOperators.py
+++ LinearStability/BuildBkgdOperators.py
@@ -16,16 +16,19 @@ def GridInterior(params, geom, discretizeVertical = False):
         halfNr, Nz  = params.halfNr, params.Nz
         N2_function = N2_profile(params.stratification_kw)
 
-        halfNz_I = eye(Nz // 2) 
+        halfNr_I = eye(halfNr)
+
+        Nz_I     = eye(Nz)
+        halfNz_I = eye(Nz // 2)
         halfNz_Z = zeros(((Nz // 2), (Nz // 2)))
         
-        rInterior       = (kron(diag(geom.r[1:(halfNr + 1)]), eye(Nz)) 
+        rInterior       = (kron(diag(geom.r[1:(halfNr + 1)]), Nz_I) 
                            + kron(diag(geom.r[1:(halfNr + 1)]), 
                                   block([[halfNz_Z, halfNz_I], 
                                         [halfNz_I, halfNz_Z]])
                                  )
                           )
-        r2RecipInterior = (kron(diag(1/geom.r[1:(halfNr+1)]**2), eye(Nz)) 
+        r2RecipInterior = (kron(diag(1/geom.r[1:(halfNr+1)]**2), Nz_I) 
                            + kron(diag(1 / geom.r[1:(halfNr + 1)]**2), 
                                   block([[halfNz_Z, halfNz_I], 
                                         [halfNz_I, halfNz_Z]])
@@ -34,21 +37,26 @@ def GridInterior(params, geom, discretizeVertical = False):
 
         quarterNr_I = eye(halfNr // 2)
         quarterNr_Z = zeros(((halfNr // 2), (halfNr // 2)))
-        zInterior   = (kron(eye(halfNr), diag(geom.z[1:(Nz+1)])) 
+        zInterior   = (kron(halfNr_I, diag(geom.z[1:(Nz+1)])) 
                        + kron(block([[quarterNr_Z, quarterNr_I], 
                                     [quarterNr_I, quarterNr_Z]]), 
                               diag(geom.z[1:(Nz+1)]))
                       )
 
         N2Recip         = diag(1 / ravel(N2_function(geom.z[1:(Nz+1)])))
-        N2RecipInterior = (kron(eye(halfNr), N2Recip) 
+        N2RecipInterior = (kron(halfNr_I, N2Recip) 
                            + kron(block([[quarterNr_Z, quarterNr_I], 
                                         [quarterNr_I, quarterNr_Z]]), 
                                   N2Recip)
                           )
         
         #Update Dz: take Kron. product of (Nr x Nr) identity matrix with Dz
-        geom.Dz = kron(eye(halfNr), geom.Dz[1:(Nz + 1), 1:(Nz + 1)])
+        Dz      = geom.Dz[1:(Nz + 1), 1:(Nz + 1)]
+        geom.Dz = (kron(halfNr_I, Dz)
+                   + kron(block([[quarterNr_Z, quarterNr_I],
+                                 [quarterNr_I, quarterNr_Z]]),
+                          Dz)
+                  )
 
         geom.rInterior = rInterior
         geom.zInterior = zInterior
