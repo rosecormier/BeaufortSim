fc3473ec5696678d21646572e8c47bf648977ab8
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-01-19 08:20:12 -0500

    Lambda function for barotropic background

--- LinearStability/Stratification.py
+++ LinearStability/Stratification.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 def N2_profile(stratification_kw, dimensional_N2 = 1):
 
-    if stratification_kw == "constant":
+    if stratification_kw == "barotropic":
+        N2_function = lambda z : dimensional_N2 * np.ones_like(z)
+
+    elif stratification_kw == "constant":
         N2_function = lambda z : dimensional_N2 * z
 
     return N2_function
