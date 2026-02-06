81c408adaffc5445fbb65d9fdaecdebec8b5c3ee
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-02-06 09:23:32 -0500

    Functions that can be included to output optional diagnostics

---
+++ LibraryOptionalSimOutputs.jl
@@ -0,0 +1,76 @@
+include("LibraryVisualization.jl")
+
+function integrate_field_upwards(i, j, k, grid, ψ, integral)
+   #Integrate an arbitrary field ψ, defined at (centre, centre, face)-points,
+   # from the surface (z[grid.Nz - 1] = 0) to the depth z[k]
+
+   integral_m = ψ[i, j, (grid.Nz + 1)] .* Δzᶜᶜᶠ(i, j, (grid.Nz + 1), grid)
+
+   for m in (grid.Nz - 1):(-1):k
+      integral_m += ψ[i, j, (m + 1)] .* Δzᶜᶜᶠ(i, j, (m + 1), grid)
+   end
+
+   integral[i, j, k] = integral_m
+end
+
+function compute_∫bdz_0_z(simulation)
+   #Evalute the integral of b w.r.t. z over the interval [0, z]
+
+   int_bdz = CenterField(simulation.model.grid)
+
+   integrate_b_upwards_op = KernelFunctionOperation{Center, Center, Center}(
+                integrate_field_upwards, simulation.model.grid, simulation.model.tracers.b, int_bdz)
+
+   compute!(integrate_b_upwards_op)
+end
+
+@inline φ_coords(grid) = @inbounds compute_polar_coords(grid)[2]
+
+@inline ∂p∂φ_over_r(grid, p) = -sin(φ_coords(grid)) * ∂x(p) + cos(φ_coords(grid)) * ∂y(p)
+
+function compute_∂p∂φ_over_r(simulation)
+   #Evaluate (1/r) times the derivative of kinematic pressure deviation (p) 
+   # w.r.t. φ
+
+   compute!(∂p∂φ_over_r(simulation.model.grid, 
+			(simulation.model.pressures.pNHS 
+			 + simulation.model.pressures.pHY′)
+	       	       )
+	   )
+end
+
+@inline ∂p∂r(grid, p) = (cos(φ_coords(grid)) * ∂x(p) 
+			 + sin(φ_coords(grid)) * ∂y(p))
+
+function compute_∂p∂r(simulation)
+   #Evaluate derivative of kinematic pressure deviation (p) w.r.t. r
+
+   compute!(∂p∂r(simulation.model.grid, (simulation.model.pressures.pNHS + 
+					 simulation.model.pressures.pHY′)
+		)
+	   )
+end
+
+@inline ∂φ_∫bdz_over_r(grid, int_bdz) = (-sin(φ_coords(grid)) * ∂x(int_bdz) 
+				         + cos(φ_coords(grid)) * ∂y(int_bdz))
+
+function compute_∂φ_∫bdz_over_r(simulation)
+   #Evaluate (1/r) times the derivative, w.r.t. φ, of the integral of b w.r.t.
+   # z, where the integral is over the interval [0, z]
+
+   int_bdz = compute_∫bdz_0_z(simulation)
+
+   compute!(∂φ_∫bdz_over_r(simulation.model.grid, int_bdz))
+end
+
+@inline ∂r_∫bdz(grid, int_bdz) = (cos(φ_coords(grid)) * ∂x(int_bdz) 
+				  + sin(φ_coords(grid)) * ∂y(int_bdz))
+
+function compute_∂r_∫bdz(simulation)
+   #Evaluate the derivative, w.r.t. r, of the integral of b w.r.t. z, where the
+   # integral is over the interval [0, z]
+
+   int_bdz = compute_∫bdz_0_z(simulation)
+
+   compute!(∂r_∫bdz(simulation.model.grid, int_bdz))
+end
