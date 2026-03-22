ba1a2ad1bf5733a3544580afd0d9a9c9ff960ee4
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-03-22 10:47:30 -0400

    Fix errors in 2D Laplacian construction

--- LinearStability/BuildLaplacian.py
+++ LinearStability/BuildLaplacian.py
@@ -19,8 +19,8 @@ def BuildLaplacian(params, geom, discretizeVertical = False):
     D2 = geom.Dr2[1:(halfNr + 1), np.arange(Nr-1, halfNr, -1)] #(pos, neg)
 
     #Quadrants of 1st-order r-derivative matrix to be retained
-    E1 = geom.Dr[1:(halfNr + 1), 1:(halfNr + 1)]              #(pos, pos)
-    E2 = geom.Dr[1:(halfNr + 1), np.arange(Nr-1, halfNr, -1)] #(pos, neg)
+    #E1 = geom.Dr[1:(halfNr + 1), 1:(halfNr + 1)]              #(pos, pos)
+    #E2 = geom.Dr[1:(halfNr + 1), np.arange(Nr-1, halfNr, -1)] #(pos, neg)
 
     if sp.issparse(geom.Dr):
         
@@ -30,9 +30,19 @@ def BuildLaplacian(params, geom, discretizeVertical = False):
         
         #Build discretized Laplacian as done in Ch.11 of Trefethen
         if discretizeVertical:
-            Lap = (sp.kron((D1 + R.dot(E1)), sp.eye(Nz))
-                   + sp.kron((D2 + R.dot(E2)), np.block([[Z, I], [I, Z]])))
+            
+            E = sp.kron((R @ geom.Dr[:, :]), sp.eye(Nz))
+            
+            Lap = sp.kron(D1, sp.eye(Nz)) + E[1:(halfNr + 1), :] + sp.kron(D2, np.block([[Z, I], [I, Z]]))
+
+            #Lap = (sp.kron((D1 + R.dot(E1)), sp.eye(Nz))
+            #       + sp.kron((D2 + R.dot(E2)), np.block([[Z, I], [I, Z]])))
         else:
+            
+            #Quadrants of 1st-order r-derivative matrix to be retained
+            E1 = geom.Dr[1:(halfNr + 1), 1:(halfNr + 1)]              #(pos, pos)
+            E2 = geom.Dr[1:(halfNr + 1), np.arange(Nr-1, halfNr, -1)] #(pos, neg)
+            
             Lap = (D1 + R.dot(E1)) + (D2 + R.dot(E2))
 
     else:
@@ -42,9 +52,14 @@ def BuildLaplacian(params, geom, discretizeVertical = False):
 
         #Build discretized Laplacian as done in Ch.11 of Trefethen
         if discretizeVertical:
-            Lap = (np.kron((D1 + np.dot(R, E1)), np.eye(Nz))
-                   + np.kron((D2 + np.dot(R, E2)), np.block([[Z, I], [I, Z]])))
+            
+            E = np.kron(np.dot(R, geom.Dr[1:halfNr+1, :]), np.eye(Nz))
+
+            Lap = np.kron(D1, np.eye(Nz))# + E[:, :] + np.kron(D2, np.block([[Z, I], [I, Z]]))
+
+            #Lap = (np.kron((D1 + np.dot(R, E1)), np.eye(Nz))
+            #       + np.kron((D2 + np.dot(R, E2)), np.block([[Z, I], [I, Z]])))
         else:
             Lap = (D1 + np.dot(R, E1)) + (D2 + np.dot(R, E2))
-
+    print("all good")
     return Lap
