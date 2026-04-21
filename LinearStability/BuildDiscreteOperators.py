e79904990af6b8c39b738e4c964ac670e493f66a
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-04-21 17:27:29 -0400

    Update 'BuildBkgdOperators' function to handle 2D BG background

--- LinearStability/BuildDiscreteOperators.py
+++ LinearStability/BuildDiscreteOperators.py
@@ -11,8 +11,22 @@ def N2_profile(stratification_kw, dimensional_N2 = 1):
 
     return N2_function
 
-def Compute_rRecip(params, geom):
-    geom.rRecip = np.diag(1 / geom.r[1:params.halfNr+1])
+def ComputeRecips(params, geom, discretizeVertical = False,
+                  nondimensional = False):
+
+    geom.rRecip = np.diag(1 / geom.r[1:(params.halfNr + 1)])
+    
+    if discretizeVertical:
+    
+        if nondimensional:
+            dimensional_N2 = 1
+        else:
+            dimensional_N2 = params.Nmax**2
+
+        N2_function  = N2_profile(params.stratification_kw, 
+                                  dimensional_N2 = dimensional_N2)
+        geom.N2      = N2_function(geom.z)
+        geom.N2Recip = 1 / geom.N2
 
 def BuildBkgdOperators(params, geom, discretizeVertical = False,
                        nondimensional = False):
@@ -22,56 +36,81 @@ def BuildBkgdOperators(params, geom, discretizeVertical = False,
     else:
         dimensional_U, dimensional_σr = params.Umax, params.σr
 
-    rTilde = np.ravel(geom.r[1:params.halfNr+1]) / dimensional_σr
+    if not discretizeVertical:
+        rTilde = np.ravel(geom.r[1:(params.halfNr + 1)]) / dimensional_σr
 
     if params.bkgd == "GM":
 
         Ψ_op = -0.5 * np.exp(-rTilde**2) * dimensional_U / dimensional_σr
-        Q_op = (-2 * np.exp(-rTilde**2) * (rTilde**2 - 2)
-                        * dimensional_U / dimensional_σr**3)
+        Q_op = (-2 * np.exp(-rTilde**2) * (rTilde**2 - 2) * dimensional_U
+                / dimensional_σr**3)
 
         geom.Ψ_op, geom.Q_op = np.diag(Ψ_op), np.diag(Q_op)
 
     elif params.bkgd == "BG":
-    
-        Ψ_opRadialFactor = (np.sqrt(2 * e) * np.exp(-rTilde**2) 
-                            * dimensional_U / dimensional_σr)
 
         if not discretizeVertical:
             
-            Q_op = (np.sqrt(32 * e) * ((rTilde**2 - 2) * np.exp(-rTilde**2)
-                    * dimensional_U / dimensional_σr**3))
+            Ψ_op = (np.sqrt(2 * e) * np.exp(-rTilde**2) 
+                    * dimensional_U / dimensional_σr)
+                    
+            Q_op = (np.sqrt(32 * e) * (rTilde**2 - 2) * np.exp(-rTilde**2)
+                    * dimensional_U / dimensional_σr**3)
+            #This is the barotropic limit, so we are implicitly neglecting a
+            # factor of 1/Bu.
 
-            geom.Ψ_op, geom.Q_op = np.diag(Ψ_opRadialFactor), np.diag(Q_op)
+            geom.Ψ_op, geom.Q_op = np.diag(Ψ_op), np.diag(Q_op)
                     
         elif discretizeVertical:
-            
+        
             f0, dimensional_σz = params.f0, params.σz
-
+        
+            r       = geom.r[1:-1]
+            rTilde  = np.ravel(r) / dimensional_σr
             r2Tilde = rTilde**2
-            z       = np.diag(geom.z)
-            zTilde  = np.diag(geom.z / dimensional_σz)
+            
+            z       = geom.z[1:-1]
+            zTilde  = np.ravel(z) / dimensional_σz
             z2Tilde = zTilde**2
-            N2Recip = np.diag(geom.N2Recip)
             
-            Ψ_opVerticalFactor = np.exp(-z2Tilde)
+            Dz = geom.Dz[1:-1, 1:-1]
             
-            Ir = np.eye(params.Nr + 1)
-            Iz = np.eye(params.Nz + 1)
+            N2Recip = np.ravel(geom.N2Recip[1:-1])
+
+            Ψ_opRadialFactor   = np.diag(np.sqrt(2 * e) * np.exp(-r2Tilde)
+                                         * dimensional_U / dimensional_σr)
+            Ψ_opVerticalFactor = np.diag(np.exp(-z2Tilde))
             
-            Q_op = (-np.sqrt(8 * e) * (dimensional_U / dimensional_σr)
-                    * np.kron(np.exp(-r2Tilde), Iz)
-                    * np.kron(Ir, np.exp(-z2Tilde))
-                    * (2 * (2 - np.kron(r2Tilde, Iz)) / dimensional_σr**2)
-                       + (f0**2 / dimensional_σz**2)
-                          * np.kron(Ir, 
-                                    N2Recip - 2 * np.matmul(z2Tilde, N2Recip) 
-                                    + np.matmul(z, np.matmul(geom.Dz, N2Recip))
-                                   )
-                   )
+            Ir = np.eye(params.Nr - 1)
+            Iz = np.eye(params.Nz - 1)
+
+            Ψ_op = np.matmul(np.kron(Ψ_opRadialFactor, Iz),
+                             np.kron(Ir, Ψ_opVerticalFactor))
+                             
+            Q_opScaleFactor = (8 * e)**0.5 * (dimensional_U / dimensional_σr**3)
             
-            Ψ_op = (np.kron(Ψ_opRadialFactor, Iz) 
-                    + np.kron(Ir, Ψ_opVerticalFactor))
+            Q_opFactor1 = np.diag(np.exp(-r2Tilde))
+            Q_opFactor2 = np.diag(np.exp(-z2Tilde))
+            
+            Q_opFactor3RadialTerm   = np.diag(2 * (rTilde**2 - 2))
+            Q_opFactor3VerticalTerm = np.diag(-(f0 * dimensional_σr 
+                                                / dimensional_σz)**2 
+                                              * (N2Recip * (1 - 2 * z2Tilde) 
+                                                 + z * np.matmul(Dz, N2Recip)
+                                                )
+                                             )
+            
+            Q_opFactor3 = (np.kron(Q_opFactor3RadialTerm, Iz)
+                           + np.kron(Ir, Q_opFactor3VerticalTerm))
+
+            Q_op = np.matmul(np.matmul(np.kron(Q_opScaleFactor * Q_opFactor1,
+                                               Iz),
+                                       np.kron(Ir, Q_opFactor2)),
+                             Q_opFactor3
+                            )
+            #N.b., in the limit of very large dimensional_σz, this construction
+            # of Q_op agrees with the 1-dimensionsal Q_op constructed in the 
+            # case of the other disjunct (!discretizeVertical).
 
             geom.Ψ_op, geom.Q_op = Ψ_op, Q_op
 
