25c0e9fccb8f48a193a076b868ad6ce90e35278f
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-05-06 09:02:41 -0400

    Condense code by using new attributes of "Parameters" class

--- LinearStability/SaveToNetCDF.py
+++ LinearStability/SaveToNetCDF.py
@@ -13,52 +13,18 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
     
     dφ = 2 * pi / params.Np           #φ-increment
     φ  = dφ * np.arange(0, params.Np) #φ-coords
-    
-    f0 = params.f0
 
     makedirs("./Data", exist_ok = True) #Create data directory if nonexistent
     
-    #Whether gen. eig. problem is 2D
-    discretizeVertical = params.discretizeVertical
-    
-    if discretizeVertical:
-        Lz, Nz = params.Lz, params.Nz
-    
-    #Whether variables are nondimensional
-    nondimensional = params.nondimensional
-    
-    if nondimensional:
-    
-        Ro = params.Ro
-    
-        if discretizeVertical:
-        
-            Bu = f"{params.Bu:.1E}"
-            
-            gridString = f"Lr{Lr:.1E}_Lz{Lz:.1E}_Nr{Nr}_Nz{Nz}"
-            
-        else:
-            Bu, gridString = "Inf", f"Lr{Lr:.1E}_Nr{Nr}"
-    
-        ncfile = Dataset(f"./Data/nondimensional_{gridString}_Ro{Ro:.1E}_Bu{Bu}_f{f0:.1E}.nc", 
-                     mode = "w", auto_complex = True)
-
+    if params.discretizeVertical:
+        Lz, Nz     = params.Lz, params.Nz
+        Bu         = f"{params.Bu:.1E}"
+        gridString = f"Lr{Lr:.1E}_Lz{Lz:.1E}_Nr{Nr}_Nz{Nz}"
     else:
-    
-        Ro = params.Umax / (params.sigmar * f0)
-    
-        if discretizeVertical:
-        
-            Bu = (params.Nmax * params.sigmaz / (f0 * params.sigmar))**2
-            Bu = f"{Bu:.1E}"
-            
-            gridString = f"Lr{Lr:.1E}_Lz{Lz:.1E}_Nr{Nr}_Nz{Nz}"
-            
-        else:
-            Bu, gridString = "Inf", f"Lr{Lr:.1E}_Nr{Nr}"
-        
-        ncfile = Dataset(f"./Data/dimensional_{gridString}_Ro{Ro:.1E}_Bu{Bu}_f{f0:.1E}.nc",
-                         mode = "w", auto_complex = True)
+        Bu, gridString = "Inf", f"Lr{Lr:.1E}_Nr{Nr}"
+
+    ncfile = Dataset(f"./Data/{params.dimString}_{gridString}_Ro{params.Ro:.1E}_Bu{Bu}_f{params.f0:.1E}.nc",
+                     mode = "w", auto_complex = True)
 
     kφs    = params.kps
     nmodes = params.nmodes
@@ -82,12 +48,9 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
     φ_var[:]    = φ
     
     #Store units
-    if nondimensional:
-        r_var.units = "times σ_r"
-    else:
-        r_var.units = "metres"
+    r_var.units = params.units["r"]
     
-    if not discretizeVertical:
+    if not params.discretizeVertical:
     
         #For 1D gen. eig. problem, also save kz-information
         
@@ -96,7 +59,7 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
         kz_var    = ncfile.createVariable("kz", float, ("kz",))
         kz_var[:] = kzs
         
-        if nondimensional:
+        if params.nondimensional:
             kz_var.units = "per σ_z"
         else:
             kz_var.units = "per metre"
@@ -124,14 +87,14 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
                                             (kz_dim, kφ_dim, r_dim, φ_dim,
                                              mode_dim))
 
-    elif discretizeVertical:
+    elif params.discretizeVertical:
     
         #For 2D gen. eig. problem, also save z-information
         z_dim    = ncfile.createDimension("z", (Nz + 1))
         z_var    = ncfile.createVariable("z", float, ("z",))
         z_var[:] = geom.z[0:(Nz + 1)]
         
-        if nondimensional:
+        if params.nondimensional:
             z_var.units = "times σ_z"
         else:
             z_var.units = "metres"
@@ -169,7 +132,7 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
         
         for mode in range(nmodes):
             
-            if discretizeVertical:
+            if params.discretizeVertical:
                                                         
                 #Evaluate and save eigen-velocities at discrete grid points
                 for ell in range(len(φ)):
@@ -197,7 +160,7 @@ def SaveToNetCDF(params, geom, dimensionalGrowthRates, dimensionalPropSpeeds,
                                                                k = kφ,
                                                                φ = φ[ell])
             
-            elif not discretizeVertical:
+            elif not params.discretizeVertical:
             
                 for kz in kzs:
                 
