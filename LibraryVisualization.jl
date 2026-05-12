362b456b52350b37861137d4836b1fbfe0c4b489
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-05-12 15:28:39 -0400

    Adapt Cartesian-->polar function to run on either CPU or GPU

--- LibraryVisualization.jl
+++ LibraryVisualization.jl
@@ -27,38 +27,46 @@ function compute_polar_coords(grid)
    return(r, φ)
 end
 
-function xy_vector_to_rφ(vx, vy, grid)
-
-   function interpolate_vx(i, j, k, grid)
-      vxCi = @views interpolate((adapt(CuArray, xnodes(grid, Center()))[i],
-                                 adapt(CuArray, ynodes(grid, Center()))[j],
-                                 adapt(CuArray, znodes(grid, Center()))[k]),
-                                vx, (Face(), Center(), Center()), grid)
-   end
-
-   function interpolate_vy(i, j, k, grid)
-      vyCj = @views interpolate((adapt(CuArray, xnodes(grid, Center()))[i],
-                                 adapt(CuArray, ynodes(grid, Center()))[j],
-                                 adapt(CuArray, znodes(grid, Center()))[k]),
-                                vy, (Center(), Face(), Center()), grid)
-   end
-
-   vxC_KernOp = KernelFunctionOperation{Center, Center, Center}(
-                                        interpolate_vx, grid)
-   vxC        = Field(vxC_KernOp)
-   vyC_KernOp = KernelFunctionOperation{Center, Center, Center}(
-                                        interpolate_vy, grid)
-   vyC        = Field(vyC_KernOp)
-
-   compute!(vxC)
-   compute!(vyC)
+function xy_vector_to_rφ(vx, vy, grid, useGPU)
 
    r, φ = compute_polar_coords(grid)
 
-   vr_BinaryOp = (vxC * cos(φ)) + (vyC * sin(φ))
-   vr          = Field(vr_BinaryOp)
-   vφ_BinaryOp = (vyC * cos(φ)) - (vxC * sin(φ))
-   vφ          = Field(vφ_BinaryOp)
+   if useGPU
+   
+      function interpolate_vx(i, j, k, grid)
+         vxCi = @views interpolate((adapt(CuArray, xnodes(grid, Center()))[i],
+                                    adapt(CuArray, ynodes(grid, Center()))[j],
+                                    adapt(CuArray, znodes(grid, Center()))[k]),
+                                   vx, (Face(), Center(), Center()), grid)
+      end
+
+      function interpolate_vy(i, j, k, grid)
+         vyCj = @views interpolate((adapt(CuArray, xnodes(grid, Center()))[i],
+                                    adapt(CuArray, ynodes(grid, Center()))[j],
+                                    adapt(CuArray, znodes(grid, Center()))[k]),
+                                   vy, (Center(), Face(), Center()), grid)
+      end
+   
+      vxC_KernOp = KernelFunctionOperation{Center, Center, Center}(
+                                        interpolate_vx, grid)
+      vxC        = Field(vxC_KernOp)
+      vyC_KernOp = KernelFunctionOperation{Center, Center, Center}(
+                                        interpolate_vy, grid)
+      vyC        = Field(vyC_KernOp)
+      
+      compute!(vxC)
+      compute!(vyC)
+
+      vr_BinaryOp = (vxC * cos(φ)) + (vyC * sin(φ))
+      vr          = Field(vr_BinaryOp)
+      vφ_BinaryOp = (vyC * cos(φ)) - (vxC * sin(φ))
+      vφ          = Field(vφ_BinaryOp)
+      
+   elseif !useGPU
+   
+      vr = @at (Center, Center, Center) (vx * cos(φ)) + (vy * sin(φ))
+      vφ = @at (Center, Center, Center) (vy * cos(φ)) - (vx * sin(φ))
+   end
 
    compute!(vr)
    compute!(vφ)
@@ -68,13 +76,13 @@ end
 function ω(u, v, w, i, j, k, Δx, Δy, Δz)
    
    ωx = @. ((w[i, j:j+1, k] - w[i, j-1:j, k]) / Δy 
-	    - (v[i, j, k:k+1] - v[i, j, k-1:k]) / Δz)
+	          - (v[i, j, k:k+1] - v[i, j, k-1:k]) / Δz)
    
    ωy = @. ((u[i, j, k:k+1] - u[i, j, k-1:k]) / Δz
-	    - (w[i:i+1, j, k] - w[i-1:i, j, k]) / Δx)
+	          - (w[i:i+1, j, k] - w[i-1:i, j, k]) / Δx)
    
    ωz = @. ((v[i:i+1, j, k] - v[i-1:i, j, k]) / Δx
-	    - (u[i, j:j+1, k] - u[i, j-1:j, k]) / Δy)
+	          - (u[i, j:j+1, k] - u[i, j-1:j, k]) / Δy)
    
    return (ωx[1] + ωx[2]) / 2, (ωy[1] + ωy[2]) / 2, (ωz[1] + ωz[2]) / 2
 end
@@ -84,17 +92,17 @@ function ωz(u, v, Δx, Δy;
 
    if !isnothing(x_idx)
       ωz = @. (((v[x_idx+1, 2:end-1, :] - v[x_idx, 2:end-1, :]) 
-	        + v[x_idx, 2:end-1, :] - v[x_idx-1, 2:end-1, :]) / (2*Δx)
-	       - (u[x_idx, 2:end, :] - u[x_idx, 1:end-1, :]) / Δy)
+                 + v[x_idx, 2:end-1, :] - v[x_idx-1, 2:end-1, :]) / (2 * Δx)
+                - (u[x_idx, 2:end, :] - u[x_idx, 1:end-1, :]) / Δy)
    
    elseif !isnothing(y_idx)
       ωz = @. ((v[2:end, y_idx, :] - v[1:end-1, y_idx, :]) / Δx
-	       - ((u[2:end-1, y_idx+1, :] - u[2:end-1, y_idx, :])
-		  + u[2:end-1, y_idx, :] - u[2:end-1, y_idx-1, :]) / (2*Δy))
+	             - ((u[2:end-1, y_idx+1, :] - u[2:end-1, y_idx, :])
+		               + u[2:end-1, y_idx, :] - u[2:end-1, y_idx-1, :]) / (2*Δy))
 
    elseif !isnothing(z_idx)
       ωz = @. ((v[2:end, 2:end-1, z_idx] - v[1:end-1, 2:end-1, z_idx]) / Δx
-              - (u[2:end-1, 2:end, z_idx] - u[2:end-1, 1:end-1, z_idx]) / Δy)
+               - (u[2:end-1, 2:end, z_idx] - u[2:end-1, 1:end-1, z_idx]) / Δy)
    end
    return ωz
 end
@@ -102,8 +110,8 @@ end
 function ζa_b(U, f, σr, σz, x, y, z)
    r2_arr = @. x^2 + y^2
    z2_arr = transpose(z.^2 .* ones(Float64, (1, length(r2_arr))))
-   ζa_b = @. (f + (2*U/σr) * (r2_arr/(σr^2) - 1)
-	      * exp(1 - r2_arr/(σr^2) - z2_arr/(σz^2)))
+   ζa_b   = @. (f + (2 * U / σr) * (r2_arr / (σr^2) - 1)
+	              * exp(1 - r2_arr / (σr^2) - z2_arr / (σz^2)))
    return ζa_b
 end
 
@@ -118,8 +126,8 @@ function ∇b(b, i, j, k, Δx, Δy, Δz)
    ∂z_b = @. (b[i, j, k:k+1] - b[i, j, k-1:k]) / Δz
    
    return ((∂x_b[1] + ∂x_b[2]) / 2, 
-	   (∂y_b[1] + ∂y_b[2]) / 2, 
-	   (∂z_b[1] + ∂z_b[2]) / 2)
+           (∂y_b[1] + ∂y_b[2]) / 2, 
+           (∂z_b[1] + ∂z_b[2]) / 2)
 end
 
 function q(u, v, w, b, f, x_idx, y_idx, z_idx, Δx, Δy, Δz)
@@ -134,7 +142,7 @@ function ∂r_q(q, x, y, i, j, k, Δx, Δy)
    ∂y_q = @. (q[i, j:j+1, k] - q[i, j-1:j, k]) / Δy
    
    r    = sqrt(x^2 + y^2) 
-   ∂r_q = @. (x*∂x_q + y*∂y_q) / r
+   ∂r_q = @. (x * ∂x_q + y * ∂y_q) / r
    
    return (∂r_q[1] + ∂r_q[2]) / 2
 end
@@ -152,8 +160,8 @@ function pad_filenames(datetime; prefix = "output")
    if length(outfile_paths) > 9
       for file_path in outfile_paths
          if length(file_path) == 38
-	    mv(file_path, replace(file_path, "_part" => "_part0"))
-	 end
+	          mv(file_path, replace(file_path, "_part" => "_part0"))
+         end
       end
    end
    
