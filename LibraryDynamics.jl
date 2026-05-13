35dcdf515455943b6dbc4b8f83e88880a6071580
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-05-13 08:38:24 -0400

    Rename kwarg

--- LibraryDynamics.jl
+++ LibraryDynamics.jl
@@ -24,18 +24,20 @@ function lognormal_strat(N²₀, N²_max, d_ML, z; σ = 0.5)
    return N², b
 end
 
-function chebyshev_spaced_faces(i, ξ_min, Nξ; ξ_max = 0.0, ξ_centre = 0.0)
+function chebyshev_spaced_faces(i, ξ_min, Nξ; ξ_max = 0.0, ξ0 = 0.0)
+
+   i -= 1
 
    Lξ = ξ_max - ξ_min
    
-   arg_shift = asin(1 + (ξ_centre/Lξ))
+   pi_shift = asin(1 + (ξ0/Lξ))
 
-   N_below_ξ_centre = (Nξ*pi) / (2*(pi-arg_shift)) 
+   N_below_ξ0 = (Nξ*pi) / (2*(pi-pi_shift)) 
 
-   if i <= N_below_ξ_centre
-      i_face = ξ_centre + Lξ * (sin((pi-arg_shift)*i/Nξ) - 1)
-   elseif i > N_below_ξ_centre
-      i_face = ξ_centre - Lξ * (sin((pi-arg_shift)*i/Nξ) - 1)
+   if i <= N_below_ξ0
+      i_face = ξ0 + Lξ * (sin((pi-pi_shift)*i/Nξ) - 1)
+   elseif i > N_below_ξ0
+      i_face = ξ0 - Lξ * (sin((pi-pi_shift)*i/Nξ) - 1)
    end
 
    return i_face
