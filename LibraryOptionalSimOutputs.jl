d3edea9edaab53cc542fb57e32fa014697e62a38
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-08-11 20:13:20 -0400

    Update polar-coordinate computations

--- LibraryOptionalSimOutputs.jl
+++ LibraryOptionalSimOutputs.jl
@@ -1,3 +1,4 @@
+include("LibraryCoordinateTransforms.jl")
 include("LibraryVisualization.jl")
 
 function integrate_field_upwards(i, j, k, grid, ψ, integral)
@@ -28,9 +29,9 @@ function compute_∫bdz_0_z(simulation)
    compute!(integrate_b_upwards_op)
 end
 
-@inline φ_coords(grid) = @inbounds compute_polar_coords(grid)[2]
+@inline φ_ccc_vals(grid) = @inbounds polar_coords_Fields(grid, "c", "c", "c")[2]
 
-@inline ∂p∂φ_over_r(grid, p) = -sin(φ_coords(grid)) * ∂x(p) + cos(φ_coords(grid)) * ∂y(p)
+@inline ∂p∂φ_over_r(grid, p) = -sin(φ_ccc_vals(grid)) * ∂x(p) + cos(φ_ccc_vals(grid)) * ∂y(p)
 
 function compute_∂p∂φ_over_r(simulation)
    #=
@@ -44,7 +45,7 @@ function compute_∂p∂φ_over_r(simulation)
 	         )
 end
 
-@inline ∂p∂r(grid, p) = cos(φ_coords(grid)) * ∂x(p) + sin(φ_coords(grid)) * ∂y(p)
+@inline ∂p∂r(grid, p) = cos(φ_ccc_vals(grid)) * ∂x(p) + sin(φ_ccc_vals(grid)) * ∂y(p)
 
 function compute_∂p∂r(simulation)
    #=
@@ -57,8 +58,8 @@ function compute_∂p∂r(simulation)
 	         )
 end
 
-@inline ∂φ_∫bdz_over_r(grid, int_bdz) = (-sin(φ_coords(grid)) * ∂x(int_bdz) 
-				         + cos(φ_coords(grid)) * ∂y(int_bdz))
+@inline ∂φ_∫bdz_over_r(grid, int_bdz) = (-sin(φ_ccc_vals(grid)) * ∂x(int_bdz) 
+				         + cos(φ_φ_ccc_vals(grid)) * ∂y(int_bdz))
 
 function compute_∂φ_∫bdz_over_r(simulation)
    #=
@@ -71,8 +72,8 @@ function compute_∂φ_∫bdz_over_r(simulation)
    compute!(∂φ_∫bdz_over_r(simulation.model.grid, int_bdz))
 end
 
-@inline ∂r_∫bdz(grid, int_bdz) = (cos(φ_coords(grid)) * ∂x(int_bdz) 
-				                          + sin(φ_coords(grid)) * ∂y(int_bdz))
+@inline ∂r_∫bdz(grid, int_bdz) = (cos(φ_ccc_vals(grid)) * ∂x(int_bdz) 
+				                          + sin(φ_ccc_vals(grid)) * ∂y(int_bdz))
 
 function compute_∂r_∫bdz(simulation)
    #=
