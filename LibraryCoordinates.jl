b4e6cbb25eca9bdee2269f3d18322f282190844d
R. Cormier <87246977+rosecormier@users.noreply.github.com>
2026-08-31 17:59:05 -0400

    Move all grid-related functions to this file

--- LibraryCoordinates.jl
+++ LibraryCoordinates.jl
@@ -1,4 +1,110 @@
-using Oceananigans.AbstractOperations, Oceananigans.Fields
+using CSV
+using Oceananigans
+using Oceananigans.AbstractOperations
+using Oceananigans.Fields
+using Tables
+
+##############################################
+# FUNCTIONS TO BUILD/SAVE OCEANANIGANS GRIDS #
+##############################################
+
+function chebyshev_spaced_faces(i, ξ_min, Nξ; ξ_max = 0.0, ξ0 = 0.0)
+
+   Lξ = ξ_max - ξ_min
+   
+   pi_shift = asin(1 + (ξ0 / Lξ))
+
+   N_below_ξ0 = ((Nξ + 1) * pi) / (2 * (pi - pi_shift)) 
+
+   if i == 1
+      i_face = ξ_min
+   elseif 1 < i <= N_below_ξ0
+      i_face = ξ0 + Lξ * (sin((pi - pi_shift) * (i - 1) / Nξ) - 1)
+   elseif i > N_below_ξ0
+      i_face = ξ0 - Lξ * (sin((pi - pi_shift) * (i - 1) / Nξ) - 1)
+   end
+
+   return i_face
+end
+
+function build_custom_z_grid(z_grid_type, Lz, Nz) 
+
+   custom_z_grids = Dict("uniform" => (-Lz, 0),
+                         "chebyshev" => k -> chebyshev_spaced_faces(k, -Lz, 
+                                                                    Nz + 1)
+                        )
+   
+   return custom_z_grids[z_grid_type]
+end
+
+function build_Oceananigans_RectilinearGrid(gridParams)
+   #=
+   Instantiate an Oceananigans RectilinearGrid that directly uses 'gridParams'.
+   Should be used as, e.g., the model grid for a simulation.
+   =#
+   
+   Nx, Ny, Nz = gridParams.Nx, gridParams.Ny, gridParams.Nz
+   Lr, Lz     = gridParams.Lr, gridParams.Lz
+   
+   grid = RectilinearGrid(gridParams.architecture,
+                          topology = gridParams.topology,
+                          size = (Nx, Ny, Nz), 
+                          x = (-Lr, Lr), 
+                          y = (-Lr, Lr), 
+                          z = build_custom_z_grid(gridParams.z_grid_type, 
+                                                  Lz, Nz),
+                          halo = (gridParams.Hx, gridParams.Hy, gridParams.Hz)
+                         )
+end
+
+function build_tall_copy_Oceananigans_RectilinearGrid(gridParams)
+   #=
+   Instantiate an Oceananigans RectilinearGrid that is in every way identical
+    to 'build_Oceananigans_RectilinearGrid(gridParams)' except that one halo
+    point at each vertical boundary is converted to an interior point.
+   I.e., the tall copy has vertical size 'gridParams.Nz + 2' and vertical halo
+    size 'gridParams.Hz - 1'.
+   =#
+   
+   regularGrid = build_Oceananigans_RectilinearGrid(gridParams)
+
+   Nx, Ny, Nz = gridParams.Nx, gridParams.Ny, gridParams.Nz
+   Hx, Hy, Hz = gridParams.Hx, gridParams.Hy, gridParams.Hz
+   Lr, Lz     = gridParams.Lr, gridParams.Lz
+
+   #Get zFace-coordinates for tall grid
+   zF = @view no_offset_view(adapt(Array, regularGrid.z.cᵃᵃᶠ))[Hz:(end - (Hz - 1))]
+
+   tallGrid = RectilinearGrid(gridParams.architecture, 
+                              topology = gridParams.topology, 
+                              size = (Nx, Ny, Nz + 2), 
+                              x = (-Lr, Lr), 
+                              y = (-Lr, Lr), 
+                              z = zF, 
+                              halo = (Hx, Hy, Hz - 1)
+                             )
+end
+
+function save_zC_values(z_grid, grid)
+   #=
+   Save zC values to a csv file, if non-existent for this grid (Chebyshev only).
+   =#
+   
+   if z_grid == "chebyshev"
+   
+      gridfilepath = joinpath("./Logs", "grid_Nz$(grid.Nz).csv") 
+
+      if !isfile(gridfilepath)
+         mkpath(dirname(gridfilepath)) #Make required path
+         @views CSV.write(gridfilepath, Tables.table(znodes(grid, Center())),
+                          header = false)
+      end
+   end
+end
+
+######################################################################
+# FUNCTIONS TO CONVERT BETWEEN CYLINDRICAL AND CARTESIAN COORDINATES #
+######################################################################
 
 @inline r_cca(i, j, k, grid) = @inbounds sqrt((grid.xᶜᵃᵃ[i])^2 + (grid.yᵃᶜᵃ[j])^2)
 @inline r_cfa(i, j, k, grid) = @inbounds sqrt((grid.xᶜᵃᵃ[i])^2 + (grid.yᵃᶠᵃ[j])^2)
